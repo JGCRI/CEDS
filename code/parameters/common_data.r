a7a2c5603cc2d66c6e84d4dabc0fc3eeb6b4fd2a
Steve Smith (PNNL-JGCRI) <ssmith@pnnl.gov>
2015-11-10 16:57:08 -0500

    Updated system to run to 2014 (updated year parameters and updated to latest BP energy stats)

--- code/parameters/common_data.r
+++ code/parameters/common_data.r
@@ -8,7 +8,7 @@ IEA_end_year  <- 2010  # Latest year of IEA data; used to compare BP and IEA
 X_IEA_end_year  <- paste0( "X", IEA_end_year  )
 
 #The set of years for the BP data extendForwards further, and is used to augment the IEA.
-BP_years <- 2011:2013 #The years for which there is only BP data
+BP_years <- 2011:2014 #The years for which there is only BP data
 X_BP_years <- paste0("X", BP_years)
 
 #The combined overall yearset.
@@ -17,5 +17,5 @@ X_emissions_years <- paste0( "X", emissions_years)
 
 start_year = 1960
 X_start_year = paste0("X",start_year)
-end_year = 2013
+end_year = 2014
 X_end_year = paste0("X",end_year)
