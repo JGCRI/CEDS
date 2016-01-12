3c702fe4c8c9e2f7605577acd4c78ed936a6d8ac
nkholod <nazar.kholod@pnnl.gov>
2016-01-12 08:41:39 -0500

    Update code to run  2015 IEA files

--- code/parameters/common_data.R
+++ code/parameters/common_data.R
@@ -1,14 +1,14 @@
 #Define historical years variables.
 
 #Set of historical years for which the CEDS system has IEA data.
-IEA_years <- 1960:2010
+IEA_years <- 1960:2013
 X_IEA_years <- paste0( "X", IEA_years)
 
-IEA_end_year  <- 2010  # Latest year of IEA data; used to compare BP and IEA
+IEA_end_year  <- 2013  # Latest year of IEA data; used to compare BP and IEA
 X_IEA_end_year  <- paste0( "X", IEA_end_year  )
 
 #The set of years for the BP data extendForwards further, and is used to augment the IEA.
-BP_years <- 2011:2014 #The years for which there is only BP data
+BP_years <- 2014 #The years for which there is only BP data
 X_BP_years <- paste0("X", BP_years)
 
 #The combined overall yearset.
@@ -19,6 +19,3 @@ start_year = 1960
 X_start_year = paste0("X",start_year)
 end_year = 2014
 X_end_year = paste0("X",end_year)
-
-# This currently takes a long time - option to turn of writting this out if not needed
-Write_value_metadata = 0
