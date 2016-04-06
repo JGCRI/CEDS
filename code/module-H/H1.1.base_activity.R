d9a0d06b7e65b656ead31b3f2a569c29a8bb91cc
Steven J Smith (PNNL-JGCRI) <ssmith@pnnl.gov>
2016-04-06 06:00:40 -0400

    Fix error (1750 instead of 1850 bound) that resulted in NAs

--- code/module-H/H1.1.base_activity.R
+++ code/module-H/H1.1.base_activity.R
@@ -46,7 +46,7 @@ shipping_fuel <- readData( 'DIAG_OUT', 'A.global_intl_shipping_fuel')
 
 ceds_activity[ paste0('X', historical_pre_extension_year: 1959)] <- NA
 ceds_activity <- ceds_activity[ c( 'iso' , 'sector' , 'fuel' , 'units' , X_extended_years ) ]
-ceds_activity[ which( ceds_activity$sector == '1A3di_International-shipping'), paste0('X',1850:1959) ] <- 0
+ceds_activity[ which( ceds_activity$sector == '1A3di_International-shipping'), paste0('X',1750:1959) ] <- 0
 
 # ---------------------------------------------------------------------------
 # 3. Add Shipping Fuel
