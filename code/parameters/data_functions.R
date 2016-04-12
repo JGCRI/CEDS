1b4fb97aaf42eb2fc99414c9413f125dfcba819b
linhdvu14 <linh.vu@pnnl.gov>
2016-04-12 12:49:12 -0400

    Small correction to interpolate_NAs()

--- code/parameters/data_functions.R
+++ code/parameters/data_functions.R
@@ -418,7 +418,7 @@ interpolate_NAs <- function( df){
     row <- df[i, ]
     if( length(rle(is.na(c(NA,row,NA)))$values)>3 ) {interpolate_rows <- c(interpolate_rows, i) }}
 
-  df[ interpolate_rows , ] <- t( na.approx(t(df[ interpolate_rows , ]))   )
+  df[ interpolate_rows , ] <- t( na.approx(t(df[ interpolate_rows , ]), na.rm = F)   )
   return (df)
 }
 
