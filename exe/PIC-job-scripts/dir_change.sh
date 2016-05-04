25083d7aabbab05ed951f7a9d35f6b2a10dbea67
Leyang Feng <leyang.feng@pnnl.gov>
2016-05-04 18:39:24 -0400

    Added scripts for running system on PIC

---
+++ exe/PIC-job-scripts/dir_change.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sed -i 's/feng999/feng999/g' *
+
+
---
+++ exe/PIC-job-scripts/make-activity.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH -A GCAM
+#SBATCH -t 10:00:00
+#SBATCH -N 1
+#SBATCH -p shared
+#SBATCH -n 1
+
+#Set up your environment you wish to run in with module commands.
+module purge
+module load R/3.2.0
+
+#Actually codes starts here
+now=$(date)
+echo "Current time : $now"
+
+cd /people/feng999/CEDS
+make activity
+
+now=$(date)
+echo "Current time : $now"
+
