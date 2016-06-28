eedf9e09795157275de627e38dd12465fd70d7bf
Rachel Hoesly <rachel.hoesly@pnnl.gov>
2016-06-28 16:05:18 -0400

    Adds scaling to Wiedinmyer inventory for waste emissions

--- code/module-C/C1.3.proc_NC_emissions_user_added_waste.R
+++ code/module-C/C1.3.proc_NC_emissions_user_added_waste.R
@@ -28,7 +28,7 @@ for ( i in 1:length( dirs ) ) {
   }
 }
 PARAM_DIR <- "../code/parameters/"
-
+ 
 # Call standard script header function to read in universal header files - 
 # provides logging, file support, and system functions - and start the script log.
 headers <- c( "data_functions.R", "analysis_functions.R", "process_db_functions.R" ) # Any additional function files required
@@ -47,6 +47,8 @@ if ( is.na( em ) ) em <- "NH3"
     waste_input <- readData( "EM_INV", "Global_Emissions_of_Pollutants_from_Open_Burning_of_Domestic_Waste", 
                              ".xlsx", sheet_selection = "Table S3", skip_rows = 1 )[ 1: 226, ]
     MCL <- readData( "MAPPINGS", "Master_Country_List" )
+    wiedinmyer <- readData( "EM_INV", "PM25_Inventory_vs_Wiedinmyer",
+                            ".xlsx", sheet_selection = "PM25_compare")
 
 # Unit conversion factors
     OC_conv <- 1/1.3  # weight to units of carbon
@@ -101,11 +103,58 @@ if ( is.na( em ) ) em <- "NH3"
     waste_input$units <- "kt"
     
 # Filter out emission being processed 
-    out <- filter( waste_input, emission == em ) %>% select( iso, sector, fuel, units, X2010 )
+    waste_inventory <- filter( waste_input, emission == em ) %>% select( iso, sector, fuel, units, X2010 )
 
 # ------------------------------------------------------------------------------
-# 3. Output
-    writeData( out, "DEFAULT_EF_IN", domain_extension = "non-combustion-emissions/", 
-               fn = paste0( "C.", em, "_NC_waste_emissions_user_added" ) )
+# 3. Match to wiedinmyer PM2.5 inventory
+    other_europe <- wiedinmyer[which( wiedinmyer$iso == 'other_europe_default'),'X2010_inv_to_wiedinmyer']
+    europe_isos <- MCL[grep('Euro',MCL$Region),'iso']
+    
+    wiedinmyer[which(is.na(wiedinmyer$X2010)),'X2010'] <- wiedinmyer[which(is.na(wiedinmyer$X2010)),'X2010_inventory']
+    
+    # match waste inventory with wiedinmyer inventory data
+    waste_inventory_wiedinmyer_match <- merge(waste_inventory, wiedinmyer[ c('iso','X2010_inv_to_wiedinmyer',"Use_for_median","Use_for_trend") ],
+                                         all.x = T) 
+    waste_inventory_wiedinmyer_match[c("Use_for_median","Use_for_trend")] <- replace(waste_inventory_wiedinmyer_match[c("Use_for_median","Use_for_trend")],
+                                                                                is.na(waste_inventory_wiedinmyer_match[c("Use_for_median","Use_for_trend")]),0)
+    
+    waste_inventory_wiedinmyer_match[which( waste_inventory_wiedinmyer_match$iso %in% europe_isos & 
+                                            waste_inventory_wiedinmyer_match$Use_for_median == 0 ), 'X2010_inv_to_wiedinmyer'] <- other_europe
+    
+    # seperate data frame into no wiedinmyer, wiedinmyer trend and wiedinmyer no trend
+    waste_inventory_no_wiedinmyer <- filter(waste_inventory_wiedinmyer_match,
+                                            Use_for_median == 0 & iso %!in% europe_isos ) %>% select( iso, sector, fuel, units, X2010 )
+    
+    waste_inventory_wiedinmyer_no_trend <- filter(waste_inventory_wiedinmyer_match,
+                                             (Use_for_median == 1 | iso %in% europe_isos ) & Use_for_trend == 0) %>% select( iso, sector, fuel, units, X2010, X2010_inv_to_wiedinmyer )
+    
+    waste_inventory_wiedinmyer_trend <- filter(waste_inventory_wiedinmyer_match, 
+                                               (Use_for_median == 1 | iso %in% europe_isos ) & Use_for_trend == 1)
+    
+    # match to inventory  - wiedinmyer but no trend
+    waste_inventory_wiedinmyer_no_trend$X2010 <-  waste_inventory_wiedinmyer_no_trend$X2010 * waste_inventory_wiedinmyer_no_trend$X2010_inv_to_wiedinmyer
+    waste_inventory_wiedinmyer_no_trend <- waste_inventory_wiedinmyer_no_trend %>% select( iso, sector, fuel, units, X2010) 
+    
+    # match to inventory  - wiedinmyer but no trend
+    waste_inventory_wiedinmyer_trend$X2010 <-  waste_inventory_wiedinmyer_trend$X2010 * waste_inventory_wiedinmyer_trend$X2010_inv_to_wiedinmyer
+    waste_inventory_wiedinmyer_trend <- waste_inventory_wiedinmyer_trend %>% select( iso, sector, fuel, units, X2010)
+    
+    waste_inventory_wiedinmyer_trend <- extend_data_on_trend_range( input_data = waste_inventory_wiedinmyer_trend,
+                                                                     driver_trend= wiedinmyer[c('iso', paste0('X', 1980:2010))],
+                                                                     start = 1980,
+                                                                     end = 2009,
+                                                                     id_match.driver = c('iso'),
+                                                                     range = 1)
+    waste_inventory_wiedinmyer_trend <- replace(waste_inventory_wiedinmyer_trend, waste_inventory_wiedinmyer_trend == 0, NA)                              
+# ------------------------------------------------------------------------------
+# 4. Output
+    writeData( waste_inventory_no_wiedinmyer, "DEFAULT_EF_IN", domain_extension = "non-combustion-emissions/", 
+               fn = paste0( "C.", em, "_NC_waste_emissions_no_wiedinmyer_user_added" ) )
+    
+    writeData( waste_inventory_wiedinmyer_no_trend, "DEFAULT_EF_IN", domain_extension = "non-combustion-emissions/", 
+               fn = paste0( "C.", em, "_NC_waste_emissions_wiedinmyer_no_trend_user_added" ) )
+    
+    writeData( waste_inventory_wiedinmyer_trend, "DEFAULT_EF_IN", domain_extension = "non-combustion-emissions/", 
+               fn = paste0( "C.", em, "_NC_waste_emissions_wiedinmyer_trend_user_added" ) )
 
 logStop()
