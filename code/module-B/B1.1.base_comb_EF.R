7e52f575ce9fa086b912d70d5dbf8cc25c520b01
Rachel Hoesly <rachel.hoesly@pnnl.gov>
2016-12-22 11:46:45 -0500

    Update CO2 estimates with liquid biofuel fraction, oxidation fraction options, comparisons to cdiac.

--- code/module-B/B1.1.base_comb_EF.R
+++ code/module-B/B1.1.base_comb_EF.R
@@ -69,7 +69,7 @@ if (em %in% c('SO2','NOx','NMVOC','BC','OC','CO','CH4','CO2','NH3','CO2')) {
   } else if( em == "NH3" ){
     scripts <- c( scripts, "B1.1.base_NH3_comb_EF.R" )
   } else if( em == "CO2" ) {
-    scripts <- c( scripts, "B1.1.base_CO2_comb_EF.R" )
+    scripts <- c( scripts, "B1.1.base_CO2_comb_EF.R" , "B1.1.2.CO2_biofuels_EF.R" )
   } else{
   # B1.1.base_OTHER_comb_EF.R processes GAINS EMF-30 efs for base emissions
   # other than SO2 and BC/OC, if emission species is SO2, BC, or OC, CO2, CH4
