63bd3d6f86cd6afa732e4b2eb366fe8da7d0e307
Ben Goldstein <dochvam@gmail.com>
2017-04-24 09:33:14 -0400

    Revert "Modified SPEW-related code to look for CEDS_Data-produced outputs"
    This reverts commit 14db41bf97e77383aaf7c4baf345e5f01dcfe720.

--- code/module-H/H1.2.add_activity_Bond_other_biomass.R
+++ code/module-H/H1.2.add_activity_Bond_other_biomass.R
@@ -39,11 +39,10 @@ if ( is.na( em ) ) em <- "NH3"
 # 1. Load files
 activity_all <- readData( 'MED_OUT',paste0('H.',em,'_total_activity_extended_db') )
 
-bcoc_historical <- readData( "EM_INV", "CD.SPEW_bcoc_historical", 
-                             domain_extension = "Bond-BCOC/", meta = T )
-sector_map <- readData( "MAPPINGS", domain_extension = "Bond/" , "CD.SPEW_sector_map", meta = F )
-iso_map <- readData( "MAPPINGS", domain_extension = "Bond/" , "CD.SPEW_iso_map", meta = F )
-fuel_map <- readData( "MAPPINGS", domain_extension = "Bond/" , "CD.SPEW_fuel_map", meta = F )
+bond_historical <- readData( "EM_INV", domain_extension = "Bond-BCOC/" ,"160227_SPEW_BCOCemission", ".xlsx" )
+iso_map <- readData( "MAPPINGS", domain_extension = "Bond/" , "Bond_country_map", meta = F )
+fuel_map <- readData( "MAPPINGS", domain_extension = "Bond/" , "Bond_fuel_map", meta = F )
+sector_map <- readData( "MAPPINGS", domain_extension = "Bond/" , "Bond_sector_map", meta = F )
 iea_start_year <- readData( 'ENERGY_IN' , 'IEA_iso_start_data', meta = F )
 un_pop <- readData( "MED_OUT" , 'A.UN_pop_master' )
 
