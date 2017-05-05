a9fd20c62faaf84073d85c528711667e6fae4324
Leyang Feng <leyang.feng@pnnl.gov>
2017-05-05 09:59:37 -0400

    Updated VOC chunking routine and functions.

--- code/module-G/G2.2.chunk_subVOC_emissions.R
+++ code/module-G/G2.2.chunk_subVOC_emissions.R
@@ -1,7 +1,7 @@
 # ------------------------------------------------------------------------------
-# Program Name: G2.2.chunk_file_generation_VOC.R
+# Program Name: G2.2.chunk_subVOC_emissions.R
 # Author(s): Leyang Feng
-# Date Last Updated: 10 June 2016
+# Date Last Updated: May 4 2017
 # Program Purpose:      
 # Input Files: 
 # Output Files: 
@@ -28,8 +28,8 @@ PARAM_DIR <- "../code/parameters/"
 # Call standard script header function to read in universal header files - 
 # provides logging, file support, and system functions - and start the script log.
 headers <- c( 'gridding_functions.R', 'data_functions.R', 'nc_generation_functions.R' ) # Any additional function files required
-log_msg <- "Generates chunk NetCDF files for speciated VOCs." # First message to be printed to the log
-script_name <- "G2.2.chunk_file_generation_VOC.R"
+log_msg <- "Generates chunk NetCDF files for subVOC emissions" # First message to be printed to the log
+script_name <- "G2.2.chunk_subVOC_emissions.R"
 
 source( paste0( PARAM_DIR, "header.R" ) )
 initialize( script_name, log_msg, headers )
@@ -38,11 +38,10 @@ initialize( script_name, log_msg, headers )
 # 0.5 Initialize gridding setups
 
 grid_resolution <- 0.5
-start_year <- 1900
-end_year <- 2014
+start_year <- 1750 
+end_year <- 1850
 chunk_years <- 50
-VOC_chunk <- T
-CEDS_version_value <- '2016-07-26'
+CEDS_gridding_version <- '2017-05-01'
 
 # basic start year/end year check 
 # if ( start_year %% chunk_years != 0 ) { stop( 'Start year must be a multiple of the chunk_years. ' ) }
@@ -60,39 +59,38 @@ if ( chunk_end_years[ length( chunk_end_years ) ] > end_year ) { chunk_end_years
 input_dir <- filePath( 'MED_OUT', 'gridded-emissions/', extension = "" )
 output_dir <- filePath( 'FIN_OUT', 'gridded-emissions/', extension = "" )
 
-# ------------------------------------------------------------------------------
-# 1. read in VOC mapping 
-VOC_mapping <- readData( 'GRIDDING', domain_extension = 'gridding_mappings/', file_name = 'VOC_id_name_mapping' )
+# VOC name mapping 
+VOC_names <- readData( domain = 'GRIDDING', domain_extension = "gridding_mappings/", 'VOC_id_name_mapping' )
 
 # ------------------------------------------------------------------------------
-# 2. For each chunk count generate nc read in list 
+# 1. For each chunk count generate nc read in list 
 
 # Define emissions species variable
-em <- 'NMVOC'
-# define gridtype -- VOC
 args_from_makefile <- commandArgs( TRUE )
-VOC_type <- args_from_makefile[ 1 ]
-if ( is.na( VOC_type ) )  VOC_type <- "VOC01"
+VOC_em <- args_from_makefile[ 1 ]
+if ( is.na( VOC_em ) ) VOC_em <- "VOC01"
 
 MODULE_G <- "../code/module-G/"
 
-# generate gridtype for VOC
-VOC_short_name <- VOC_mapping[ VOC_mapping$VOC_id == VOC_type, 'VOC_name' ]
-VOC_short_name <- substring( VOC_short_name, 1, 10 )
-VOC_gridtype <- paste0( VOC_type, '-', VOC_short_name )
-
-
 for ( chunk_count_index in 1 : chunk_count ) {
   
-  annual2chunk( em, grid_resolution, gridtype = VOC_gridtype, 
-                chunk_start_years, chunk_end_years, chunk_count_index, 
-                input_dir, output_dir, 
-                VOC_chunk, VOC_info = VOC_mapping, CEDS_version = CEDS_version_value,
-                output_format = 'singleVar' )  
-}
+  singleVarChunking_subVOCemissions( VOC_em, 
+                                     grid_resolution, 
+                                     chunk_start_years, 
+                                     chunk_end_years, 
+                                     chunk_count_index, 
+                                     input_dir, 
+                                     output_dir, 
+                                     gridding_version = CEDS_gridding_version,
+                                     VOC_names )
+  }
+
 
 # -----------------------------------------------------------------------------
-# 3. Stop 
+# 2. Stop 
 
 # Every script should finish with this line:
 logStop()  
+
+
+
