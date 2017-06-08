fbb49f7134221edd68c1daa9385c52333bf2181e
Ben Goldstein <dochvam@gmail.com>
2017-06-08 16:46:02 -0400

    Fixed issue in S1.1 where file was logged as created even though it was replaced

--- code/module-S/S1.1.write_summary_data.R
+++ code/module-S/S1.1.write_summary_data.R
@@ -78,19 +78,19 @@ final_emissions <- final_emissions[ -which( final_emissions$sector %in% empty_se
 
 # save shipping and aviation emissions
 bunker_emissions <- final_emissions[ which( final_emissions$sector %in% 
-                    c( "1A3ai_International-aviation", "1A3aii_Domestic-aviation",
-                       "1A3di_International-shipping" ) ) , ]
+                                              c( "1A3ai_International-aviation", "1A3aii_Domestic-aviation",
+                                                 "1A3di_International-shipping" ) ) , ]
 
 # remove international shipping and aviation emissions
 final_emissions <- final_emissions[ -which( final_emissions$sector %in% 
-                   c( "1A3ai_International-aviation", "1A3aii_Domestic-aviation",
-                      "1A3di_International-shipping" ) ) , ]
+                                              c( "1A3ai_International-aviation", "1A3aii_Domestic-aviation",
+                                                 "1A3di_International-shipping" ) ) , ]
 
 # add summary sectors
 final_emissions$summary_sector <- Master_Sector_Level_map[match(final_emissions$sector,
-                                  Master_Sector_Level_map$working_sectors_v1),'aggregate_sectors']
+                                                                Master_Sector_Level_map$working_sectors_v1),'aggregate_sectors']
 bunker_emissions$summary_sector <- Master_Sector_Level_map[match(bunker_emissions$sector,
-                                  Master_Sector_Level_map$working_sectors_v1),'aggregate_sectors']
+                                                                 Master_Sector_Level_map$working_sectors_v1),'aggregate_sectors']
 
 #simplify units
 # This assumes units are correct. Need to add a more comprehensive unit conversion function (since drivers will come in various units)
@@ -139,32 +139,32 @@ Summary_Emissions <- Summary_Emissions[ with( Summary_Emissions, order( fuel ) )
 
 # Total Emissions by Sector and Country
 Em_by_Country_Sector <- aggregate(final_emissions[X_write_years],
-                               by=list(iso=final_emissions$iso,
-                                       sector=final_emissions$summary_sector,
-                                       em= final_emissions$em,
-                                       units=final_emissions$units),sum )
+                                  by=list(iso=final_emissions$iso,
+                                          sector=final_emissions$summary_sector,
+                                          em= final_emissions$em,
+                                          units=final_emissions$units),sum )
 #Sort
 Em_by_Country_Sector <- Em_by_Country_Sector[ with( Em_by_Country_Sector, order( iso , sector ) ), ]
 
 # Emissions by country and CEDS sector
 if ( WRITE_CEDS_SECTORS ) {
-	# Total Emissions by CEDS Sector and Country
-	Em_by_Country_CEDS_Sector <- aggregate(final_emissions[X_write_years],
-								   by=list(iso=final_emissions$iso,
-										   sector=final_emissions$sector,
-										   em= final_emissions$em,
-										   units=final_emissions$units),sum )
-	#Sort
-	Em_by_Country_CEDS_Sector <- Em_by_Country_CEDS_Sector[ with( Em_by_Country_CEDS_Sector, order( iso , sector ) ), ]
-
-	# Global Emissions by CEDS Sector 
-	Em_by_CEDS_Sector <- aggregate(final_emissions[X_write_years],
-								   by=list(sector=final_emissions$sector,
-										   em= final_emissions$em,
-										   units=final_emissions$units),sum )
-	#Sort
-	Em_by_CEDS_Sector <- Em_by_CEDS_Sector[ with( Em_by_CEDS_Sector, order( sector ) ), ]
-
+  # Total Emissions by CEDS Sector and Country
+  Em_by_Country_CEDS_Sector <- aggregate(final_emissions[X_write_years],
+                                         by=list(iso=final_emissions$iso,
+                                                 sector=final_emissions$sector,
+                                                 em= final_emissions$em,
+                                                 units=final_emissions$units),sum )
+  #Sort
+  Em_by_Country_CEDS_Sector <- Em_by_Country_CEDS_Sector[ with( Em_by_Country_CEDS_Sector, order( iso , sector ) ), ]
+  
+  # Global Emissions by CEDS Sector 
+  Em_by_CEDS_Sector <- aggregate(final_emissions[X_write_years],
+                                 by=list(sector=final_emissions$sector,
+                                         em= final_emissions$em,
+                                         units=final_emissions$units),sum )
+  #Sort
+  Em_by_CEDS_Sector <- Em_by_CEDS_Sector[ with( Em_by_CEDS_Sector, order( sector ) ), ]
+  
 }
 
 # ---------------------------------------------------------------------------
@@ -172,38 +172,38 @@ if ( WRITE_CEDS_SECTORS ) {
 # Compare emissions summary from the current run and the last run. If values 
 # change over a threshold, move last-run files to previous-versions, write out 
 # current-run files, and write out comparison diagnostics.
-  FILENAME_POSTSCRIPT <- paste( "_v", substr( Sys.Date(), 1, 4 ), substr( Sys.Date(), 6, 7 ), 
-                                substr( Sys.Date(), 9, 10 ), sep = "_" )  # "_v_yyyy_mm_dd"
+FILENAME_POSTSCRIPT <- paste( "_v", substr( Sys.Date(), 1, 4 ), substr( Sys.Date(), 6, 7 ), 
+                              substr( Sys.Date(), 9, 10 ), sep = "_" )  # "_v_yyyy_mm_dd"
 
 # Create output folders (if not already exist) and define values
-  dir.create( "../final-emissions/current-versions", showWarnings = F )
-  dir.create( "../final-emissions/previous-versions", showWarnings = F )
-  dir.create( "../final-emissions/diagnostics", showWarnings = F )
-  base_fn <- paste0( "CEDS_", em ,"_emissions_by_country_sector" )
-  summary_fn <- paste0( "CEDS_", em , "_emissions_by_country_sector", FILENAME_POSTSCRIPT )
-  summary_fn1 <- paste0( "CEDS_", em , "_emissions_by_country", FILENAME_POSTSCRIPT )
-  summary_fn2 <- paste0( "CEDS_", em , "_global_emissions_by_fuel", FILENAME_POSTSCRIPT )
-  summary_fn3 <- paste0( "CEDS_", em , "_emissions_by_country_CEDS_sector", FILENAME_POSTSCRIPT )
-  summary_fn4 <- paste0( "CEDS_", em , "_global_emissions_by_CEDS_sector", FILENAME_POSTSCRIPT )
-  THRESHOLD_PERCENT <- 1
-  
+dir.create( "../final-emissions/current-versions", showWarnings = F )
+dir.create( "../final-emissions/previous-versions", showWarnings = F )
+dir.create( "../final-emissions/diagnostics", showWarnings = F )
+base_fn <- paste0( "CEDS_", em ,"_emissions_by_country_sector" )
+summary_fn <- paste0( "CEDS_", em , "_emissions_by_country_sector", FILENAME_POSTSCRIPT )
+summary_fn1 <- paste0( "CEDS_", em , "_emissions_by_country", FILENAME_POSTSCRIPT )
+summary_fn2 <- paste0( "CEDS_", em , "_global_emissions_by_fuel", FILENAME_POSTSCRIPT )
+summary_fn3 <- paste0( "CEDS_", em , "_emissions_by_country_CEDS_sector", FILENAME_POSTSCRIPT )
+summary_fn4 <- paste0( "CEDS_", em , "_global_emissions_by_CEDS_sector", FILENAME_POSTSCRIPT )
+THRESHOLD_PERCENT <- 1
+
 # Define function to write summary files
-  writeSummary <- function() {
-    printLog( "Write emissions summary" )
-    writeData( Em_by_Country_Sector, "FIN_OUT", summary_fn, domain_extension = "current-versions/", meta = F )
-    writeData( Em_by_Country, "FIN_OUT", summary_fn1, domain_extension = "current-versions/", meta = F )
-    writeData( Summary_Emissions, "FIN_OUT", summary_fn2, domain_extension = "current-versions/", meta = F )
-    if ( WRITE_CEDS_SECTORS ) { 
-      writeData( Em_by_Country_CEDS_Sector, "FIN_OUT", summary_fn3, domain_extension = "current-versions/", meta = F )
-      writeData( Em_by_CEDS_Sector, "FIN_OUT", summary_fn4, domain_extension = "current-versions/", meta = F )
-    }
+writeSummary <- function() {
+  printLog( "Write emissions summary" )
+  writeData( Em_by_Country_Sector, "FIN_OUT", summary_fn, domain_extension = "current-versions/", meta = F )
+  writeData( Em_by_Country, "FIN_OUT", summary_fn1, domain_extension = "current-versions/", meta = F )
+  writeData( Summary_Emissions, "FIN_OUT", summary_fn2, domain_extension = "current-versions/", meta = F )
+  if ( WRITE_CEDS_SECTORS ) { 
+    writeData( Em_by_Country_CEDS_Sector, "FIN_OUT", summary_fn3, domain_extension = "current-versions/", meta = F )
+    writeData( Em_by_CEDS_Sector, "FIN_OUT", summary_fn4, domain_extension = "current-versions/", meta = F )
   }
-  
+}
+
 # If no summary file exists, write out current-run files and exit
 if ( length( list.files( "../final-emissions/current-versions/", pattern = paste0( "_", em, "_" ) ) ) == 0 ) {
   writeSummary()
-
-# Else compare current-run and last-run emissions summary
+  
+  # Else compare current-run and last-run emissions summary
 } else {
   printLog( "Compare emissions summary from current run and last run" )
   
@@ -211,12 +211,9 @@ if ( length( list.files( "../final-emissions/current-versions/", pattern = paste
   dir.create( paste0( "../final-emissions/", em, "_last-run" ), showWarnings = F )
   fl <- list.files( "../final-emissions/current-versions/", pattern = paste0( "_", em, "_" ), full.names = T )
   moveFileList( fl, paste0( "../final-emissions/", em, "_last-run/" ) )
-
-  # write out current-run
-  writeSummary()
   
-  # read current-run and last-run emissions summary
-  em_current <- readData( "FIN_OUT", summary_fn, domain_extension = "current-versions/", meta = F )
+  # read last-run emissions summary and gather descriptor info for comparison
+  em_current <- Em_by_Country_Sector
   em_last_fn <- list.files( paste0( "../final-emissions/", em, "_last-run/" ), pattern = base_fn ) %>% file_path_sans_ext()
   em_last <- readData( "FIN_OUT", paste0( em, "_last-run/", em_last_fn ), meta = F )
   id_cols <- names( em_current )[ !grepl( "X", names( em_current ) ) ]
@@ -225,82 +222,84 @@ if ( length( list.files( "../final-emissions/current-versions/", pattern = paste
   # if current-run and last-run have different ID columns, do nothing
   if ( any( sort( id_cols ) != sort( id_cols_last ) ) ) {
     warning( paste( "Current and last versions of", base_fn, 
-                     "have different ID columns. Cannot run comparison." ) )
-  
-  # if current-run and last-run are identical, delete current-run and move last-run to current-versions/
+                    "have different ID columns. Cannot run comparison." ) )
+    
+    # if current-run and last-run are identical, don't write out 
+    #    current-run and move last-run back to current-versions
   } else if ( identical( em_current, em_last ) ) {
     warning( paste( base_fn, "did not change from last run." ) )
-    unlink( dir( "../final-emissions/current-versions/", 
-                 pattern = paste0( "_", em, "_" ), full.names = T ) )  
     fl <- list.files( paste0( "../final-emissions/", em, "_last-run/" ), pattern = paste0( "_", em, "_" ), full.names = T )
     moveFileList( fl, "../final-emissions/current-versions/" )
-
-  # else run comparison diagnostics
+    
+    # else write the new summary and run comparison diagnostics
   } else {
+    
+    writeSummary()
+    
     # delete relevant emissions from previous-versions and diagnostics
-      unlink( dir( "../final-emissions/previous-versions/", 
-                   pattern = paste0( "_", em, "_" ), full.names = T ) )
-      unlink( dir( "../final-emissions/diagnostics/", 
-                   pattern = paste0( "_", em, "_" ), full.names = T ) )
-      
+    unlink( dir( "../final-emissions/previous-versions/", 
+                 pattern = paste0( "_", em, "_" ), full.names = T ) )
+    unlink( dir( "../final-emissions/diagnostics/", 
+                 pattern = paste0( "_", em, "_" ), full.names = T ) )
+    
     # move content of last-run to previous-versions
-      fl <- list.files( paste0( "../final-emissions/", em, "_last-run" ), full.names = T )
-      moveFileList( fl, "../final-emissions/previous-versions/" )
+    fl <- list.files( paste0( "../final-emissions/", em, "_last-run" ), full.names = T )
+    moveFileList( fl, "../final-emissions/previous-versions/" )
     
     # make df of added/dropped data
-      dropped_rows <- em_last[ do.call( paste0, em_last[ id_cols ] ) %!in%
-                                do.call( paste0, em_current[ id_cols ] ), ]
-      added_rows <- em_current[ do.call( paste0, em_current[ id_cols ] ) %!in%
-                              do.call( paste0, em_last[ id_cols ] ), ]
-      dropped_cols <- em_last[, names( em_last ) %in% id_cols | 
-                                    names( em_last ) %!in% names( em_current ) ]
-      added_cols <- em_current[, names( em_current ) %in% id_cols | 
-                                 names( em_current ) %!in% names( em_last ) ]
-      
+    dropped_rows <- em_last[ do.call( paste0, em_last[ id_cols ] ) %!in%
+                               do.call( paste0, em_current[ id_cols ] ), ]
+    added_rows <- em_current[ do.call( paste0, em_current[ id_cols ] ) %!in%
+                                do.call( paste0, em_last[ id_cols ] ), ]
+    dropped_cols <- em_last[, names( em_last ) %in% id_cols | 
+                              names( em_last ) %!in% names( em_current ) ]
+    added_cols <- em_current[, names( em_current ) %in% id_cols | 
+                               names( em_current ) %!in% names( em_last ) ]
+    
     # compare current-run and last-run
-      em_current <- melt( em_current, id = id_cols )
-      names( em_current )[ names( em_current ) %in% c( "variable", "value" ) ] <-
-        c( "year", "current" )
-      em_last <- melt( em_last, id = id_cols )
-      names( em_last )[ names( em_last ) %in% c( "variable", "value" ) ] <-
-        c( "year", "previous" ) 
-      em_comp <- merge( em_current, em_last ) %>% 
-        mutate( diff = current - previous,
-                change_percent = diff*100 / current )
-      em_comp$change_percent[ em_comp$current == em_comp$previous ] <- 0
-      
+    em_current <- melt( em_current, id = id_cols )
+    names( em_current )[ names( em_current ) %in% c( "variable", "value" ) ] <-
+      c( "year", "current" )
+    em_last <- melt( em_last, id = id_cols )
+    names( em_last )[ names( em_last ) %in% c( "variable", "value" ) ] <-
+      c( "year", "previous" ) 
+    em_comp <- merge( em_current, em_last ) %>% 
+      mutate( diff = current - previous,
+              change_percent = diff*100 / current )
+    em_comp$change_percent[ em_comp$current == em_comp$previous ] <- 0
+    
     # make df of where current-run and last-run differ by more than THRESHOLD_PERCENT 
-      em_comp_out <- filter( em_comp, abs( change_percent ) >= THRESHOLD_PERCENT )
-      
+    em_comp_out <- filter( em_comp, abs( change_percent ) >= THRESHOLD_PERCENT )
+    
     # make df of absolute difference in wide format
-      x_var <- id_cols[ "year" %!in% id_cols ] %>%
-        lapply( function(x) paste0( x, "+" ) ) %>% paste( collapse = '' )
-      x_var <- substr( x_var, 0, nchar( x_var ) - 1 )
-      abs_diff <- filter( em_comp, diff != 0 ) %>% 
-        cast( as.formula( paste( x_var, "year", sep = "~" ) ), value = "diff" )
-      
+    x_var <- id_cols[ "year" %!in% id_cols ] %>%
+      lapply( function(x) paste0( x, "+" ) ) %>% paste( collapse = '' )
+    x_var <- substr( x_var, 0, nchar( x_var ) - 1 )
+    abs_diff <- filter( em_comp, diff != 0 ) %>% 
+      cast( as.formula( paste( x_var, "year", sep = "~" ) ), value = "diff" )
+    
     # make df of absolute difference as percentage of country total 
-      abs_diff_percent <- filter( em_comp, diff != 0 ) %>%
-        mutate( diff_percent = round( diff*100 / current ) ) %>%
-        cast( as.formula( paste( x_var, "year", sep = "~" ) ), value = "diff_percent" )
-
+    abs_diff_percent <- filter( em_comp, diff != 0 ) %>%
+      mutate( diff_percent = round( diff*100 / current ) ) %>%
+      cast( as.formula( paste( x_var, "year", sep = "~" ) ), value = "diff_percent" )
+    
     # write out diagnostics
-      if ( nrow( dropped_rows ) > 0 )
-        writeData( dropped_rows, "FIN_OUT", paste0( "diagnostics/", summary_fn, "_dropped-rows" ), meta = F )
-      if ( nrow( added_rows ) > 0 )
-        writeData( added_rows, "FIN_OUT", paste0( "diagnostics/", summary_fn, "_added-rows" ), meta = F )
-      if ( ncol( dropped_cols ) > length( id_cols ) )
-        writeData( dropped_cols, "FIN_OUT", paste0( "diagnostics/", summary_fn, "_dropped-cols" ), meta = F )
-      if ( ncol( added_cols ) > length( id_cols ) )
-        writeData( added_cols, "FIN_OUT", paste0( "diagnostics/", summary_fn, "_added-cols" ), meta = F )
-      if ( nrow( em_comp_out ) > 0 )
-        writeData( em_comp_out, "FIN_OUT", paste0( "diagnostics/", summary_fn, "_comparison" ), meta = F )
-      if ( !all( em_comp$diff == 0 ) ) {
-        abs_diff[ is.na( abs_diff ) ] <- ""
-        abs_diff_percent[ is.na( abs_diff_percent ) ] <- ""
-        writeData( abs_diff, "FIN_OUT", paste0( "diagnostics/", summary_fn, "_diff" ), meta = F )
-        writeData( abs_diff_percent, "FIN_OUT", paste0( "diagnostics/", summary_fn, "_diff-percent" ), meta = F )
-      }
+    if ( nrow( dropped_rows ) > 0 )
+      writeData( dropped_rows, "FIN_OUT", paste0( "diagnostics/", summary_fn, "_dropped-rows" ), meta = F )
+    if ( nrow( added_rows ) > 0 )
+      writeData( added_rows, "FIN_OUT", paste0( "diagnostics/", summary_fn, "_added-rows" ), meta = F )
+    if ( ncol( dropped_cols ) > length( id_cols ) )
+      writeData( dropped_cols, "FIN_OUT", paste0( "diagnostics/", summary_fn, "_dropped-cols" ), meta = F )
+    if ( ncol( added_cols ) > length( id_cols ) )
+      writeData( added_cols, "FIN_OUT", paste0( "diagnostics/", summary_fn, "_added-cols" ), meta = F )
+    if ( nrow( em_comp_out ) > 0 )
+      writeData( em_comp_out, "FIN_OUT", paste0( "diagnostics/", summary_fn, "_comparison" ), meta = F )
+    if ( !all( em_comp$diff == 0 ) ) {
+      abs_diff[ is.na( abs_diff ) ] <- ""
+      abs_diff_percent[ is.na( abs_diff_percent ) ] <- ""
+      writeData( abs_diff, "FIN_OUT", paste0( "diagnostics/", summary_fn, "_diff" ), meta = F )
+      writeData( abs_diff_percent, "FIN_OUT", paste0( "diagnostics/", summary_fn, "_diff-percent" ), meta = F )
+    }
   }
   
   # delete the temp folder last-run
@@ -311,7 +310,7 @@ if ( length( list.files( "../final-emissions/current-versions/", pattern = paste
 
 # source figure and comparison files to print figures
 source('../code/diagnostic/Figures.R') 
-  
+
 if (em != 'CO2')  source('../code/diagnostic/Compare_to_RCP.R')
 if( em %!in% c( 'CO2', 'NH3' ) )  source('../code/diagnostic/Compare_to_GAINS.R')
 
