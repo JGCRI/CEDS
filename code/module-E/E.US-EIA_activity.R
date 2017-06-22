a47583c6a7b97fc5ef044e52b1c980988460dc4f
Ben Goldstein <dochvam@gmail.com>
2017-06-22 12:26:43 -0400

    Final cast form; no adjustments or comparisons conducted

--- code/module-E/E.US-EIA_activity.R
+++ code/module-E/E.US-EIA_activity.R
@@ -1,14 +1,13 @@
 #------------------------------------------------------------------------------
 # Program Name: E.US-EIA_activity.R
-# Authors' Names: Tyler Pitkanen, Jon Seibert, Rachel Hoesly, Steve Smith, Ryan Bolt
-# Date Last Modified: January 20, 2016
-# Program Purpose: To read in & reformat Argentina emissions inventory data
-# This data only contains data from 1990 - 2011, missing data from 2000 and 2010.
-# Units are initially in metric tonnes
-# Input Files: Argentina_Translation.xlsx, Argentina Inventario 1990-2012-ipcc1996.xlsx, 
-# Output Files: E.[em]_ARG_inventory.csv
+# Author: Ben Goldstein
+# Date Last Modified: June 22, 2017
+# Program Purpose: To read in & reformat EIA activity data from 1949 to 2014
+# Units are initially in btu
+# Input Files: all files in the folder input/activity/EIA-data
+# Output Files: E.[em]_EIA_activity.csv
 # Notes: 
-# TODO: Re-write read-in so that order of years is taken from input data instead of assumed.
+
 # ------------------------------------------------------------------------------
 # 0. Read in global settings and headers
 
@@ -68,6 +67,8 @@ initialize( script_name, log_msg, headers )
     
     coal_conversion <- readData( "MER_TA5", domain = "ACTIVITY_IN", 
                                  domain_extension = "EIA-data/unit-conversion/")
+    petrol_conversion <- readData( "MER_TA3", domain = "ACTIVITY_IN",
+                                   domain_extension = "EIA-data/unit-conversion/")
 
 # ------------------------------------------------------------------------------
 # 3. Basic reformatting
@@ -90,47 +91,132 @@ initialize( script_name, log_msg, headers )
 # ------------------------------------------------------------------------------
 # 4. Fuel mapping
     
-    EIA_data_formatted$agg_fuel <- NA
+    EIA_data_formatted$fuel <- NA
     
-    EIA_data_formatted$agg_fuel[ grep( "Coal", EIA_data_formatted$Description ) ] <- "coal"
-    EIA_data_formatted$agg_fuel[ grep( "Biomass", EIA_data_formatted$Description ) ] <- "biomass"
-    EIA_data_formatted$agg_fuel[ grep( "Natural Gas", EIA_data_formatted$Description ) ] <- "gas"
-    EIA_data_formatted$agg_fuel[ grep( "Petroleum", EIA_data_formatted$Description ) ] <- "oil"
+    EIA_data_formatted$fuel[ grep( "Coal", EIA_data_formatted$Description ) ] <- "coal"
+    EIA_data_formatted$fuel[ grep( "Biomass", EIA_data_formatted$Description ) ] <- "biomass"
+    EIA_data_formatted$fuel[ grep( "Natural Gas", EIA_data_formatted$Description ) ] <- "gas"
+    EIA_data_formatted$fuel[ grep( "Petroleum", EIA_data_formatted$Description ) ] <- "oil"
     
-    data_unused <- EIA_data_formatted[ is.na( EIA_data_formatted$agg_fuel ), ]
+    data_unused <- EIA_data_formatted[ is.na( EIA_data_formatted$fuel ), ]
     printLog(paste0(nrow(data_unused), " valid annual datapoints were not mapped to CEDS fuels"))
     
-    EIA_data_formatted <- EIA_data_formatted[ !is.na( EIA_data_formatted$agg_fuel ), ]
+    EIA_data_formatted <- EIA_data_formatted[ !is.na( EIA_data_formatted$fuel ), ]
     
     
 # ------------------------------------------------------------------------------
 # 4. Sector mapping
     
-    EIA_data_formatted$agg_sector <- NA
+    EIA_data_formatted$sector <- NA
     
-    EIA_data_formatted$agg_sector[ grep( "Residential Sector", 
+    EIA_data_formatted$sector[ grep( "Residential Sector", 
                                          EIA_data_formatted$Description ) ] <- "Residential"
-    EIA_data_formatted$agg_sector[ grep( "Commercial Sector", 
+    EIA_data_formatted$sector[ grep( "Commercial Sector", 
                                          EIA_data_formatted$Description ) ] <- "Commercial"
-    EIA_data_formatted$agg_sector[ grep( "Industrial Sector", 
+    EIA_data_formatted$sector[ grep( "Industrial Sector", 
                                          EIA_data_formatted$Description ) ] <- "Industry"
-    EIA_data_formatted$agg_sector[ grep( "Transportation Sector", 
+    EIA_data_formatted$sector[ grep( "Transportation Sector", 
                                          EIA_data_formatted$Description ) ] <- "Transportation"
-    EIA_data_formatted$agg_sector[ grep( "Electric Power Sector", 
+    EIA_data_formatted$sector[ grep( "Electric Power Sector", 
                                          EIA_data_formatted$Description ) ] <- "Energy Transf/Ext"
     
-    data_unused <- EIA_data_formatted[ is.na( EIA_data_formatted$agg_sector ), ]
+    data_unused <- EIA_data_formatted[ is.na( EIA_data_formatted$sector ), ]
     printLog( paste0( nrow( data_unused ), " valid annual datapoints were not mapped to CEDS sectors" ) )
     
-    EIA_data_formatted <- EIA_data_formatted[ !is.na(EIA_data_formatted$agg_fuel), ]
+    EIA_data_formatted <- EIA_data_formatted[ !is.na(EIA_data_formatted$fuel), ]
     
     EIA_data_formatted$iso <- "usa"
     
-    EIA_data_formatted <- EIA_data_formatted[ , c( "iso", "agg_sector", "agg_fuel", 
+    EIA_data_formatted <- EIA_data_formatted[ , c( "iso", "sector", "fuel", 
                                                    "Unit", "year", "Value" ) ]
     
+
+# ------------------------------------------------------------------------------
+# 4. Prepare unit conversion
+    
+    coal_conversion$year <- paste0( "X", substr( coal_conversion$YYYYMM, 1, 4 ) )
+    petrol_conversion$year <- paste0( "X", substr( petrol_conversion$YYYYMM, 1, 4 ) )
+    
+    petrol_conversion$sector <- NA
+    petrol_conversion$sector[ which( petrol_conversion$MSN == "PARCKUS") ] <- "Residential"
+    petrol_conversion$sector[ which( petrol_conversion$MSN == "PACCKUS") ] <- "Commercial"
+    petrol_conversion$sector[ which( petrol_conversion$MSN == "PAICKUS") ] <- "Industry"
+    petrol_conversion$sector[ which( petrol_conversion$MSN == "PAACKUS") ] <- "Transportation"
+    petrol_conversion$sector[ which( petrol_conversion$MSN == "PAEIKUS") ] <- "Energy Transf/Ext"
+
+    coal_conversion$sector <- NA
+    coal_conversion$sector[ which( coal_conversion$MSN == "CLHCKUS") ] <- "Residential and Commercial"
+    coal_conversion$sector[ which( coal_conversion$MSN == "CLOCKUS") ] <- "Industry"
+    coal_conversion$sector[ which( coal_conversion$MSN == "CLEIKUS") ] <- "Energy Transf/Ext"
+
+    coal_conversion_res <- coal_conversion[ which( coal_conversion$sector == 
+                                                     "Residential and Commercial"), ]
+    coal_conversion_res$sector <- "Residential"
+    coal_conversion_com <- coal_conversion[ which( coal_conversion$sector == 
+                                                     "Residential and Commercial"), ]
+    coal_conversion_com$sector <- "Commercial"
+    coal_conversion_trans <- coal_conversion[ which( coal_conversion$sector == 
+                                                     "Industry"), ]
+    coal_conversion_trans$sector <- "Transportation"
     
     
+    coal_conversion <- rbind( coal_conversion, coal_conversion_com, coal_conversion_res,
+                              coal_conversion_trans)
+    
+    petrol_conversion <- petrol_conversion[ !is.na(petrol_conversion$sector), ] 
+    coal_conversion <- coal_conversion[ !is.na(coal_conversion$sector), ] 
+    coal_conversion$fuel <- "coal"
+    petrol_conversion$fuel <- "oil"
+    
+# Convert from (million btu/short ton) to TJ/kt
+    petrol_conversion$Value <- as.numeric(petrol_conversion$Value) * 1.163
+    petrol_conversion$Unit <- "TJ/kt"
+    
+    coal_conversion$Value <- as.numeric(coal_conversion$Value) * 1.163
+    coal_conversion$Unit <- "TJ/kt"
+    
+    conversion_factors <- rbind( coal_conversion, petrol_conversion )
+    
+# ------------------------------------------------------------------------------
+# 4. Execute conversion to kt
+  
+    EIA_data_formatted$Value[ which( EIA_data_formatted$fuel == "biomass" ) ] <-
+          as.numeric( EIA_data_formatted$Value[ which( EIA_data_formatted$fuel == "biomass" ) ] ) / 
+                  conversionFactor_biomass_kt_TJ
+    EIA_data_formatted$Unit[ which( EIA_data_formatted$fuel == "biomass" ) ] <- "kt"
+    
+    EIA_data_formatted$Value[ which( EIA_data_formatted$fuel == "gas" ) ] <-
+          as.numeric( EIA_data_formatted$Value[ which( EIA_data_formatted$fuel == "gas" ) ] ) * 
+                  conversionFactor_naturalgas_TJ_per_kt
+    EIA_data_formatted$Unit[ which( EIA_data_formatted$fuel == "gas" ) ] <- "kt"
+    
+    
+    EIA_convert_subset <- EIA_data_formatted[ which( EIA_data_formatted$fuel %in%
+                                                       c( "oil", "coal" ) ), ]
+    
+    EIA_convert_subset <- left_join( EIA_convert_subset, 
+                                     conversion_factors[ , c( "Value", "year", "fuel", "sector" ) ],
+                                     by = c( "fuel", 
+                                             "sector",
+                                             "year" ) )
+    colnames(EIA_convert_subset)[6:7] <- c("Value", "Conversion_factor")
+    
+    EIA_convert_subset$Value <- as.numeric(EIA_convert_subset$Value) / EIA_convert_subset$Conversion_factor
+    EIA_convert_subset$Unit <- "kt"
+    
+
+    EIA_data_formatted[ which( EIA_data_formatted$fuel %in%
+                                                       c( "oil", "coal" ) ), ] <-
+            EIA_convert_subset[, c("iso", "sector", "fuel",
+                                   "Unit", "year", "Value")]
+    
+
+# ------------------------------------------------------------------------------
+# 4. Cast to wide and write output
+    
+    EIA_final <- spread(EIA_data_formatted, key=year, value=Value)
+    
+    writeData( EIA_final, domain="MED_OUT", paste0('E.',em,'_US-EIA_inventory'))
     
     
     
