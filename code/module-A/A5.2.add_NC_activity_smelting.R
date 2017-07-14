92e8ed666a7449109c4e487274504599be14492e
Ben Goldstein <bg5087a@american.edu>
2017-07-14 16:01:04 -0400

    A5.2 Code Review

--- code/module-A/A5.2.add_NC_activity_smelting.R
+++ code/module-A/A5.2.add_NC_activity_smelting.R
@@ -43,55 +43,58 @@
 # 0.5. Settings
 
 # Input file
-input_name <- "Smelter-Feedstock-Sulfur"
-input_ext <- ".xlsx"
-input <- paste0( input_name, input_ext )
+    input_name <- "Smelter-Feedstock-Sulfur"
+    input_ext <- ".xlsx"
+    input <- paste0( input_name, input_ext )
 
 # Location of input files relative to wd (input)
-input_domain <- "ACTIVITY_IN"
+    input_domain <- "ACTIVITY_IN"
 
 # ------------------------------------------------------------------------------
 # 1. Read in files
 
 # iso_mapping <- readData( "MAPPINGS","2011_NC_SO2_ctry" )
-act_input <- readData( "MAPPINGS", "activity_input_mapping")
-MSL <- readData( "MAPPINGS", "Master_Fuel_Sector_List", ".xlsx", sheet_selection = "Sectors" )
-
-activity_data <- readData( input_domain, input_name, input_ext )
+    act_input <- readData( "MAPPINGS", "activity_input_mapping")
+    MSL <- readData( "MAPPINGS", "Master_Fuel_Sector_List", ".xlsx", 
+                     sheet_selection = "Sectors" )
+    
+    activity_data <- readData( input_domain, input_name, input_ext )
 
 # Set variables with the activity of the input file and the associated units
-activity_name <- act_input$activity[ act_input$file == input ]
+    activity_name <- act_input$activity[ act_input$file == input ]
 
-#Check that this activity is actually used, otherwise don't add
-if ( activity_name %in% MSL$activity ) {
+# Check that this activity is actually used, otherwise don't add
+    if ( activity_name %in% MSL$activity ) {
+    
+    ### Do we need to check that this is only one?
+        unit <- unique( MSL$units[ MSL$activity == activity_name ] )
+  
+# ------------------------------------------------------------------------------
+# 2. Reformatting
+  
+    # Apply reformatting header function
+        activity_data <- cleanData( activity_data )
+  
+    # Apply activity and unit assignments, and reorders columns to standard form
+        activity_data$activity <- activity_name
+        activity_data$units <- unit
+        results <- cbind( activity_data[ c( "iso", "activity", "units" ) ] , 
+                          activity_data[ 2:( length( activity_data ) - 3 ) ] )
+  
+    # Sort results by iso and activity
+        results <- results[ with( results, order( iso, activity ) ), ]
+  
+# ------------------------------------------------------------------------------
+# 3. Output
+    
+    # Add reformatted activity_data to the activity database, extending or truncating it as necessary.
+    # By default, it will be extended forward to the common end year, but not backwards.
+    # Only do this if the activityCheck header function determines that the activities in
+    # the reformatted activity_data are all present in the Master List.
+        if ( activityCheck( results, check_all = FALSE ) ) {
+              addToActivityDb( results )
+        }
+    }
 
-  unit <- unique( MSL$units[ MSL$activity == activity_name ] )
-  
-  # ------------------------------------------------------------------------------
-  # 2. Reformatting
-  
-  # Apply reformatting header function
-  activity_data <- cleanData( activity_data )
-  
-  # Applies activity and unit assignments, and reorders columns to standard form
-  activity_data$activity <- activity_name
-  activity_data$units <- unit
-  results <- cbind( activity_data[ c( "iso","activity","units" ) ] , activity_data[ 2:( length( activity_data ) - 3 ) ] )
-  
-  # Sort results by iso and activity
-  results <- results[ with( results, order( iso, activity ) ), ]
-  
-  # ------------------------------------------------------------------------------
-  # 3. Output
-  
-  # Add reformatted activity_data to the activity database, extending or truncating it as necessary.
-  # By default, it will be extended forward to the common end year, but not backwards.
-  # Only do this if the activityCheck header function determines that the activities in
-  # the reformatted activity_data are all present in the Master List.
-  if( activityCheck( results, check_all = FALSE ) ){
-      addToActivityDb( results )
-  }
-}
-
-logStop()
+    logStop()
 # END
