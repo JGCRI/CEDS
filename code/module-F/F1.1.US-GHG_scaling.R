6b2b83b8772a18a24a54ef41e82b17a493be050f
Ben Goldstein <bg5087a@american.edu>
2017-08-10 10:02:34 -0700

    Module F CR

--- code/module-F/F1.1.US-GHG_scaling.R
+++ code/module-F/F1.1.US-GHG_scaling.R
@@ -15,52 +15,53 @@
 
 # Set working directory to the CEDS “input” directory and define PARAM_DIR as the
 # location of the CEDS “parameters” directory relative to the new working directory.
-dirs <- paste0( unlist( strsplit( getwd(), c( '/', '\\' ), fixed = T ) ), '/' )
-for ( i in 1:length( dirs ) ) {
-  setwd( paste( dirs[ 1:( length( dirs ) + 1 - i ) ], collapse = '' ) )
-  wd <- grep( 'CEDS/input', list.dirs(), value = T )
-  if ( length(wd) > 0 ) {
-    setwd( wd[1] )
-    break
-  }
-} 
-PARAM_DIR <- "../code/parameters/"
-
+    dirs <- paste0( unlist( strsplit( getwd(), c( '/', '\\' ), fixed = T ) ), '/' )
+    for ( i in 1:length( dirs ) ) {
+        setwd( paste( dirs[ 1:( length( dirs ) + 1 - i ) ], collapse = '' ) )
+        wd <- grep( 'CEDS/input', list.dirs(), value = T )
+        if ( length(wd) > 0 ) {
+            setwd( wd[1] )
+            break
+        }
+    } 
+    PARAM_DIR <- "../code/parameters/"
+    
 # Get emission species first so can name log appropriately
-args_from_makefile <- commandArgs( TRUE )
-em <- args_from_makefile[1]
-if ( is.na( em ) ) em <- "CH4"
+    args_from_makefile <- commandArgs( TRUE )
+    em <- args_from_makefile[1]
+    if ( is.na( em ) ) em <- "CH4"
 
 # Call standard script header function to read in universal header files - 
 # provide logging, file support, and system functions - and start the script log.
-headers <- c( 'common_data.R',"data_functions.R" ,"emissions_scaling_functions.R", "analysis_functions.R",
-              "interpolation_extention_functions.R" ) # Additional function files required.
-log_msg <- "US-EPA inventory scaling" # First message to be printed to the log
-script_name <- paste0(em,"-F1.1.US-GHG_scaling.R")
+    headers <- c( 'common_data.R', "data_functions.R", 
+                  "emissions_scaling_functions.R", "analysis_functions.R",
+                  "interpolation_extention_functions.R" ) # Additional function files required.
+    log_msg <- "US-EPA inventory scaling" # First message to be printed to the log
+    script_name <- paste0( em, "-F1.1.US-GHG_scaling.R" )
 
-source( paste0( PARAM_DIR, "header.R" ) )
-initialize( script_name, log_msg, headers )
+    source( paste0( PARAM_DIR, "header.R" ) )
+    initialize( script_name, log_msg, headers )
 
 # ------------------------------------------------------------------------------
 # 1. Define parameters for inventory specific script
 
 # Stop script if running for unsupported species
-if ( em %!in% c( 'CH4' ) ) {
-  stop (paste( 'US scaling is not supported for emission species ', em, '. Remove from script
-               list in F1.1.inventory_scaling.R'))
-}
+    if ( em %!in% c( 'CH4' ) ) {
+       stop( paste( 'US scaling is not supported for emission species ', 
+                    em, '. Remove from script list in F1.1.inventory_scaling.R' ) )
+    }
 
 # For each Module E script, define the following parameters:
 # Inventory parameters. Provide the inventory and mapping file names, the
 #   mapping method (by sector, fuel, or both), and the regions covered by
 #   the inventory (as a vector of iso codes)
-inv_name <- 'US-GHG' #for naming diagnostic files
-region <- c( "usa" ) 
-sector_fuel_mapping <- 'US-GHG_scaling_mapping'
-mapping_method <- 'sector'
-inv_years<- 1990:2014
-inventory_data_file <- paste0('E.',em,'_',inv_name,'_inventory')
-inv_data_folder <- 'MED_OUT'
+    inv_name <- 'US-GHG' #for naming diagnostic files
+    region <- c( "usa" ) 
+    sector_fuel_mapping <- 'US-GHG_scaling_mapping'
+    mapping_method <- 'sector'
+    inv_years <- 1990:2014
+    inventory_data_file <- paste0( 'E.', em, '_', inv_name, '_inventory' )
+    inv_data_folder <- 'MED_OUT'
 
 # ------------------------------------------------------------------------------
 # 2. Read In Data with scaling functions
@@ -68,43 +69,44 @@ inv_data_folder <- 'MED_OUT'
 # Read in the inventory data, mapping file, the specified emissions species, and
 # the latest versions of the scaled EFs  
 
-scaling_data <- F.readScalingData( inventory = inventory_data_file, inv_data_folder,
-                                   mapping = sector_fuel_mapping, method = mapping_method,
-                                   region, inv_name, inv_years)
-list2env(scaling_data , envir = .GlobalEnv) 
+    scaling_data <- F.readScalingData( inventory = inventory_data_file, 
+                                       inv_data_folder,
+                                       mapping = sector_fuel_mapping, 
+                                       method = mapping_method,
+                                       region, inv_name, inv_years )
+    list2env( scaling_data, envir = .GlobalEnv )
 
 
 # ------------------------------------------------------------------------------
 # 3. Arrange the CEDS emissions data to match the inventory data
 
 # Aggregate inventory data to scaling sectors/fuels 
-inv_data <- F.invAggregate( std_form_inv , region )
+    inv_data <- F.invAggregate( std_form_inv, region )
 
 # Aggregate ceds data to scaling sectors/fuels
-ceds_data <- F.cedsAggregate( input_em, region, mapping_method )
+    ceds_data <- F.cedsAggregate( input_em, region, mapping_method )
 
 # ------------------------------------------------------------------------------
 # 4. Calculate Scaling Factors, reaggregate to CEDS sectors  
 
 # Calculate and extend scaling factors
-scaling_factors_list <- F.scaling( ceds_data, inv_data, region, 
-                                   replacement_method = 'replace', 
-                                   max_scaling_factor = 100,
-                                   replacement_scaling_factor = 100)
-list2env(scaling_factors_list , envir = .GlobalEnv)
+    scaling_factors_list <- F.scaling( ceds_data, inv_data, region, 
+                                       replacement_method = 'replace', 
+                                       max_scaling_factor = 100,
+                                       replacement_scaling_factor = 100 )
+    list2env( scaling_factors_list, envir = .GlobalEnv )
 
 # Apply Scaling Factors to Ceds data
-scaled <- F.applyScale(scaling_factors)
-scaled_ef <- scaled[[1]]
-scaled_em <- scaled[[2]]
+    scaled <- F.applyScale( scaling_factors )
+    scaled_ef <- scaled[[ 1 ]]
+    scaled_em <- scaled[[ 2 ]]
 
 # ------------------------------------------------------------------------------
 # 5. Encorporate scaled em and EF and
-# Write Scaled emissions and emission factors
+#    Write Scaled emissions and emission factors
 
-F.addScaledToDb( scaled_ef, scaled_em, meta_notes )
-
+    F.addScaledToDb( scaled_ef, scaled_em, meta_notes )
 
 # Every script should finish with this line
-logStop()
+    logStop()
 # END
