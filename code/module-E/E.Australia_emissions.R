a9470293e6b55e86f6f7b8ae24986d884c9f9b3a
Ben Goldstein <bg5087a@american.edu>
2017-08-10 10:06:16 -0700

    Module E code review

--- code/module-E/E.Australia_emissions.R
+++ code/module-E/E.Australia_emissions.R
@@ -12,121 +12,118 @@
 
 # Set working directory to the CEDS "input" directory and define PARAM_DIR as the
 # location of the CEDS "parameters" directory, relative to the new working directory.
-dirs <- paste0( unlist( strsplit( getwd(), c( '/', '\\' ), fixed = T ) ), '/' )
-for ( i in 1:length( dirs ) ) {
-  setwd( paste( dirs[ 1:( length( dirs ) + 1 - i ) ], collapse = '' ) )
-  wd <- grep( 'CEDS/input', list.dirs(), value = T )
-  if ( length(wd) > 0 ) {
-    setwd( wd[1] )
-    break
-  }
-} 
-PARAM_DIR <- "../code/parameters/"
+    dirs <- paste0( unlist( strsplit( getwd(), c( '/', '\\' ), fixed = T ) ), '/' )
+    for ( i in 1:length( dirs ) ) {
+        setwd( paste( dirs[ 1:( length( dirs ) + 1 - i ) ], collapse = '' ) )
+        wd <- grep( 'CEDS/input', list.dirs(), value = T )
+        if ( length( wd ) > 0 ) {
+            setwd( wd[ 1 ] )
+            break
+        }
+    } 
+    PARAM_DIR <- "../code/parameters/"
 
 # Get emission species first so can name log appropriately
-args_from_makefile <- commandArgs( TRUE )
-em <- args_from_makefile[1]
-if ( is.na( em ) ) em <- "NOx"
+    args_from_makefile <- commandArgs( TRUE )
+    em <- args_from_makefile[ 1 ]
+    if ( is.na( em ) ) em <- "NOx"
   
 # Call standard script header function to read in universal header files - 
 # provide logging, file support, and system functions - and start the script log.
-headers <- c( 'common_data.R',"data_functions.R" , "analysis_functions.R") # Additional function files required.
-log_msg <- "Generating Australia emission inventory data" # First message to be printed to the log
-script_name <- paste0(em,"-E.Australia_emission.R")
-
-source( paste0( PARAM_DIR, "header.R" ) )
-initialize( script_name, log_msg, headers )
+    headers <- c( 'common_data.R', "data_functions.R", 
+                  "analysis_functions.R" ) # Additional function files required.
+    log_msg <- "Generating Australia emission inventory data" # First message to be printed to the log
+    script_name <- paste0( em, "-E.Australia_emission.R" )
+    
+    source( paste0( PARAM_DIR, "header.R" ) )
+    initialize( script_name, log_msg, headers )
 
 # ------------------------------------------------------------------------------
-# 1. Define parameters for inventory specific script
-
-# Stop script if running for unsupported species
-#if ( em %!in% c( 'SO2', 'NOx', 'NMVOC', 'CO' ) ) {
-#  stop (paste( ' Australia scaling is not supported for emission species', em, 'remove from script
-#               list in F1.1.inventory_scaling.R') )
-#}
+# 1. Define parameters for inventory-specific script
 
 # For each Module E script, define the following parameters:
 # Inventory parameters. Provide the inventory and mapping file names, the
 #   mapping method (by sector, fuel, or both), and the regions covered by
 #   the inventory (as a vector of iso codes)
-inventory_data_file <- paste0( 'Australia/', em, '_Australia_UNFCCC_and_NPI' )
-inv_data_folder <- "EM_INV"
-sector_fuel_mapping <- 'Australia_scaling_mapping'
-mapping_method <- 'sector'
-inv_name <- 'AUS' #for naming diagnostic files
-region <- c( "aus" ) 
-inv_years<-c( 2000, 2006, 2012 )
-
-
+    inventory_data_file <- paste0( 'Australia/', em, 
+                                   '_Australia_UNFCCC_and_NPI' )
+    inv_data_folder <- "EM_INV"
+    sector_fuel_mapping <- 'Australia_scaling_mapping'
+    mapping_method <- 'sector'
+    inv_name <- 'AUS' #for naming diagnostic files
+    region <- c( "aus" ) 
+    inv_years<-c( 2000, 2006, 2012 )
 
 # ------------------------------------------------------------------------------
 # 2 Inventory in Standard Form (iso-sector-fuel-years, iso-sector-years, etc)
 
-# write out a dummy file unsupported species
-if ( em %!in% c( 'SO2', 'NOx', 'NMVOC', 'CO' ) ) {
-  inv_data <- data.frame( )
-
-  } else {
-# Import Sheets containing 2008,2010,2012 data.
-sheet_name <- "1999-2000"
-inv_data_2000 <- readData( inv_data_folder, inventory_data_file , ".xlsx", sheet_selection = sheet_name )
-sheet_name <- "2005-2006"
-inv_data_2006 <- readData( inv_data_folder, inventory_data_file , ".xlsx", sheet_selection = sheet_name )
-sheet_name <- "2011-2012"
-inv_data_2012 <- readData( inv_data_folder, inventory_data_file , ".xlsx", sheet_selection = sheet_name )
-
-# kepp desired columns
-keep_columns <- c( "NPI Sector", "NPI - Total (kt)" )
-drop_rows <- c( 'TOTAL', 'Total', 'total' )
-
-df2000 <- subset( inv_data_2000, select = keep_columns )
-col_names <- c( 'sector', 'X2000')
-colnames( df2000 ) <- col_names
-df2000 <- df2000[ ! df2000$sector %in% drop_rows , ]
-
-df2006 <- subset( inv_data_2006, select = keep_columns )
-col_names <- c( 'sector', 'X2006')
-colnames( df2006 ) <- col_names
-df2006 <- df2006[ ! df2006$sector %in% drop_rows , ]
-
-df2012 <- subset( inv_data_2012, select = keep_columns )
-col_names <- c( 'sector', 'X2012')
-colnames( df2012 ) <- col_names
-df2012 <- df2012[ ! df2012$sector %in% drop_rows , ]
-
-
-# construct unified sectors in each df
-df2000 <- merge( df2000, df2006['sector'], by = 'sector', all = T )
-df2000 <- merge( df2000, df2012['sector'], by = 'sector', all = T )
-df2006 <- merge( df2006, df2000['sector'], by = 'sector', all = T )
-df2012 <- merge( df2012, df2000['sector'], by = 'sector', all = T )
-
-# Putting data for specific emission into a single dataframe.
-sector <- as.character( df2000$sector )
-X2000 <- df2000[ , 2 ]
-X2006 <- df2006[ , 2 ]
-X2012 <- df2012[ , 2 ]
-
-inv_data <- data.frame(sector, X2000, X2006, X2012, stringsAsFactors = F ) 
-
-# Clean rows and columns to standard format
-inv_data$iso <- 'aus'
-inv_data$unit <- 'kt'
-inv_data <- inv_data[ , c( 'iso', 'sector', 'unit', paste0( 'X', inv_years ) ) ]
-inv_data[ is.na( inv_data ) ] <- 0
-
-}
+    if ( em %!in% c( 'SO2', 'NOx', 'NMVOC', 'CO' ) ) {
+    # Output a blank df for unsupported species
+        inv_data <- data.frame( )
+    
+    } else {
+    # Import Sheets containing data.
+        sheet_name <- "1999-2000"
+        inv_data_2000 <- readData( inv_data_folder, inventory_data_file,
+                                   ".xlsx", sheet_selection = sheet_name )
+        sheet_name <- "2005-2006"
+        inv_data_2006 <- readData( inv_data_folder, inventory_data_file, 
+                                   ".xlsx", sheet_selection = sheet_name )
+        sheet_name <- "2011-2012"
+        inv_data_2012 <- readData( inv_data_folder, inventory_data_file, 
+                                   ".xlsx", sheet_selection = sheet_name )
+        
+    # Identify desired columns
+        keep_columns <- c( "NPI Sector", "NPI - Total (kt)" )
+        drop_rows <- c( 'TOTAL', 'Total', 'total' )
+        
+    # Drop unnecessary columns and rows in each dataframe
+        df2000 <- subset( inv_data_2000, select = keep_columns )
+        col_names <- c( 'sector', 'X2000' )
+        colnames( df2000 ) <- col_names
+        df2000 <- df2000[ !df2000$sector %in% drop_rows , ] ### use %!in%
+        
+        df2006 <- subset( inv_data_2006, select = keep_columns )
+        col_names <- c( 'sector', 'X2006' )
+        colnames( df2006 ) <- col_names
+        df2006 <- df2006[ !df2006$sector %in% drop_rows, ] ### use %!in%
+        
+        df2012 <- subset( inv_data_2012, select = keep_columns )
+        col_names <- c( 'sector', 'X2012' )
+        colnames( df2012 ) <- col_names
+        df2012 <- df2012[ !df2012$sector %in% drop_rows, ] ### use %!in%
+        
+    
+    # Make sure all dfs have all columns
+        df2000 <- merge( df2000, df2006[ 'sector' ], by = 'sector', all = T )
+        df2000 <- merge( df2000, df2012[ 'sector' ], by = 'sector', all = T )
+        df2006 <- merge( df2006, df2000[ 'sector' ], by = 'sector', all = T )
+        df2012 <- merge( df2012, df2000[ 'sector' ], by = 'sector', all = T )
+    
+    # Combine all data into a single dataframe; one em, three years
+        sector <- as.character( df2000$sector )
+        X2000 <- df2000[ , 2 ]
+        X2006 <- df2006[ , 2 ]
+        X2012 <- df2012[ , 2 ]
+        inv_data <- data.frame( sector, X2000, X2006, X2012, 
+                                stringsAsFactors = F ) 
+    
+    # Clean rows and columns to standard format
+        inv_data$iso <- 'aus'
+        inv_data$unit <- 'kt'
+        inv_data <- inv_data[ , c( 'iso', 'sector', 'unit', 
+                                   paste0( 'X', inv_years ) ) ]
+        inv_data[ is.na( inv_data ) ] <- 0
+    
+    }
 
 # ------------------------------------------------------------------------------
 # 5. Output
 # write standard form inventory
-writeData( inv_data , domain = "MED_OUT", paste0('E.',em,'_',inv_name,'_inventory') )
+    writeData( inv_data, domain = "MED_OUT", 
+               paste0( 'E.', em, '_', inv_name, '_inventory' ) )
 # Every script should finish with this line
-logStop()
+    logStop()
 # END
-
-
-
 
 
--- code/module-E/E.US-GHG_emissions.R
+++ code/module-E/E.US-GHG_emissions.R
@@ -12,82 +12,92 @@
 
 # Set working directory to the CEDS “input” directory and define PARAM_DIR as the
 # location of the CEDS “parameters” directory relative to the new working directory.
-dirs <- paste0( unlist( strsplit( getwd(), c( '/', '\\' ), fixed = T ) ), '/' )
-for ( i in 1:length( dirs ) ) {
-  setwd( paste( dirs[ 1:( length( dirs ) + 1 - i ) ], collapse = '' ) )
-  wd <- grep( 'CEDS/input', list.dirs(), value = T )
-  if ( length(wd) > 0 ) {
-    setwd( wd[1] )
-    break
-  }
-} 
-PARAM_DIR <- "../code/parameters/"
+    dirs <- paste0( unlist( strsplit( getwd(), c( '/', '\\' ), fixed = T ) ), '/' )
+    for ( i in 1:length( dirs ) ) {
+        setwd( paste( dirs[ 1:( length( dirs ) + 1 - i ) ], collapse = '' ) )
+        wd <- grep( 'CEDS/input', list.dirs(), value = T )
+        if ( length(wd) > 0 ) {
+            setwd( wd[1] )
+            break
+        }
+    } 
+    PARAM_DIR <- "../code/parameters/"
 
 # Get emission species first so can name log appropriately
-args_from_makefile <- commandArgs( TRUE )
-em <- args_from_makefile[1]
-if ( is.na( em ) ) em <- "CH4"
+    args_from_makefile <- commandArgs( TRUE )
+    em <- args_from_makefile[1]
+    if ( is.na( em ) ) em <- "CH4"
   
 # Call standard script header function to read in universal header files - 
 # provide logging, file support, and system functions - and start the script log.
-headers <- c( 'common_data.R',"data_functions.R" ) # Additional function files required.
-log_msg <- "Initial reformatting of US GHG emissions" # First message to be printed to the log
-script_name <- "E.US-GHG_emissions.R"
-
-source( paste0( PARAM_DIR, "header.R" ) )
-initialize( script_name, log_msg, headers )
+    headers <- c( 'common_data.R',"data_functions.R" ) # Additional function files required.
+    log_msg <- "Initial reformatting of US GHG emissions" # First message to be printed to the log
+    script_name <- "E.US-GHG_emissions.R"
+    
+    source( paste0( PARAM_DIR, "header.R" ) )
+    initialize( script_name, log_msg, headers )
 
 # ------------------------------------------------------------------------------
 # 1. Define parameters for inventory specific script
 
-inventory_data_file <- 'USA/US_GHG_inventory'
-inv_data_folder <- "EM_INV"
-inv_name <- 'US_GHG' #for naming diagnostic files
-inv_years<-c(1990:2014)
+    inventory_data_file <- 'USA/US_GHG_inventory'
+    inv_data_folder <- "EM_INV"
+    inv_name <- 'US_GHG' #for naming diagnostic files
+    inv_years<-c( 1990:2014 )
 
 # ------------------------------------------------------------------------------
-# 1.5. Inventory in Standard Form (iso-sector-fuel-years, iso-sector-years, etc)
+# 2. Inventory in Standard Form (iso-sector-fuel-years, iso-sector-years, etc)
 
 # Import Sheet
-inv_data_sheet <- readData( inv_data_folder, inventory_data_file) 
-
-ghg_em <- c('CO2','N2O','CH4','NF3','SF6','PFCs','HFCs')
+    inv_data_sheet <- readData( inv_data_folder, inventory_data_file ) 
+    
+    ghg_em <- c( 'CO2', 'N2O', 'CH4', 'NF3', 'SF6', 'PFCs', 'HFCs' )
 
 # Process given emission if inventory data exists
-if ( em %in% ghg_em ){
-
-# select rows and columns 
-  inv_data <- inv_data_sheet[3:nrow(inv_data_sheet),2:ncol(inv_data_sheet)]
-  names(inv_data) <- inv_data_sheet[2,2:ncol(inv_data_sheet)]
-  names(inv_data)[1] <- 'Source'
-  inv_data <- inv_data[-grep("Does not exceed",inv_data[,1]):-nrow(inv_data_sheet),]
-
-# Define emission species
-  inv_data$EM <- NA
-  inv_data[inv_data$Source %in% ghg_em,'EM'] <- inv_data[inv_data$Source %in% ghg_em,'Source']
-  inv_data <- fill(inv_data, EM)
-  
-# remove totals
-  inv_data <- filter(inv_data, Source %!in% ghg_em, EM %in% em)
-# add X to year columns
-  inv_data <- inv_data[c('Source',paste(inv_years))]
-  names(inv_data) <- c('sector', paste0('X', inv_years))
-# convert to numeric
-  inv_data[paste0('X', inv_years)] <- apply(X=inv_data[paste0('X', inv_years)],2,FUN = gsub,pattern = ',',replacement='')
-  inv_data[paste0('X', inv_years)] <- apply(X=inv_data[paste0('X', inv_years)],2,as.numeric)
-#replace NAs with 0
-  inv_data <- replace(inv_data, is.na(inv_data), 0) 
-   
-# Write out blank df if no inventory data exists for given emission
-} else  {
-  inv_data <- data.frame()
-}
+    if ( em %in% ghg_em ) {
+    
+    # select rows and columns 
+        inv_data <- inv_data_sheet[ 3:nrow( inv_data_sheet ),
+                                    2:ncol( inv_data_sheet ) ]
+        names( inv_data ) <- inv_data_sheet[ 2, 2:ncol( inv_data_sheet ) ]
+        names( inv_data )[ 1 ] <- 'Source'
+        inv_data <- inv_data[ -grep( "Does not exceed", 
+                                     inv_data[ , 1 ] ):-nrow( inv_data_sheet ), ]
+    
+    # Define emission species
+        inv_data$EM <- NA
+        inv_data[ inv_data$Source %in% ghg_em, 'EM' ] <- 
+                  inv_data[ inv_data$Source %in% ghg_em, 'Source' ]
+        inv_data <- fill( inv_data, EM )
+      
+    # remove totals
+        inv_data <- filter( inv_data, Source %!in% ghg_em, EM %in% em )
+    # add X to year columns
+        inv_data <- inv_data[ c( 'Source', paste( inv_years ) ) ]
+        names( inv_data ) <- c( 'sector', paste0( 'X', inv_years ) )
+        
+    # convert to numeric
+        inv_data[ paste0( 'X', inv_years ) ] <-
+                  apply( X = inv_data[ paste0( 'X', inv_years ) ], 
+                         2, FUN = gsub, pattern = ',', replacement = '' )
+        inv_data[ paste0( 'X', inv_years ) ] <-
+                  apply( X = inv_data[ paste0( 'X', inv_years ) ], 
+                         2, as.numeric )
+        
+    # replace NAs with 0
+        inv_data <- replace( inv_data, is.na( inv_data ), 0 )
+       
+    # Write out blank df if no inventory data exists for given emission
+    } else {
+        inv_data <- data.frame()
+    }
 
 
 # ------------------------------------------------------------------------------
-# 2. Write standard form inventory
-writeData( inv_data , domain = "MED_OUT", paste0('E.',em,'_',inv_name,'_inventory'))
+# 3. Write standard form inventory
+    writeData( inv_data, domain = "MED_OUT", 
+               paste0( 'E.', em, '_', inv_name, '_inventory' ) )
 
 # Every script should finish with this line
-logStop()
+    logStop()
 # END
