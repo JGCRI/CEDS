7690985377f7beb4605ef879056d3644da7992cf
prestech <presley.muwan@pnnl.gov>
2017-11-22 14:46:10 -0500

    Change from using xlsx package  to openxlsx package, for producing Excel formatted diagnostic summary files.

--- code/parameters/global_settings.R
+++ code/parameters/global_settings.R
@@ -16,7 +16,8 @@ loadPackage<-function(package){
     stop(paste( "Couldn't load '", package, "'. Please Install.\n" ,sep=""))
   }
 }
-libs <- c( "ggplot2", "magrittr", "pbapply", "plyr", "dplyr", "reshape", "stringr", "XML", 
+
+libs <- c( "ggplot2", "magrittr", "pbapply", "plyr", "dplyr", "reshape", "stringr", "openxlsx","XML", 
            "readxl", 'zoo', 'gridExtra', 'tidyr')
 for( i in seq_along(libs)){
     package <- libs[[ i ]]
