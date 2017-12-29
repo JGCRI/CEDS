0c14b87e872282cc20be6973a591eb6ba12ff460
Caleb Braun <caleb.braun@pnnl.gov>
2017-12-29 10:48:21 -0500

    Refactor

--- code/parameters/user_data_inclusion_functions.R
+++ code/parameters/user_data_inclusion_functions.R
@@ -25,90 +25,80 @@ normalizeAndIncludeData <- function( Xyears, data_to_use, user_dataframe_subset,
                                      all_activity_data, override_normalization,
                                      agg_level, filename, specified_breakdowns ) {
 
-# The any() function returns NA if no elements are true but some are NA. We do a
-# quick overwrite if this is the case so that we can use
-# any(override_normalization) as a logical element without fear.
-    if ( is.na( any( override_normalization ) ) ) {
-        override_normalization <- F
+    override_normalization <- any( override_normalization, na.rm = T )
+
+    # For each aggregate level, column names are specified that will guide
+    # normalization and processing. Because of this flexible architecture,
+    # specific aggregate levels can be easily adjusted or added without changing
+    # the design of the function. However, there may only be one column in
+    # cols_given that isn't present in normalizeTo; you can only ever normalize
+    # by a single "level" of aggregation. This is a law of the function, not of
+    # the process, and may be worth changing down the line.
+    aggLevelToCols <- function(agg_level) {
+        switch(agg_level,
+            c("iso", "agg_fuel"),                                           # 1
+            c("iso", "agg_fuel", "CEDS_fuel"),                              # 2
+            c("iso", "agg_fuel", "CEDS_fuel", "agg_sector"),                # 3
+            c("iso", "agg_fuel", "CEDS_fuel", "agg_sector", "CEDS_sector"), # 4
+            c("iso", "agg_fuel", "agg_sector", "CEDS_sector"),              # 5
+            c("iso", "agg_fuel", "agg_sector")                              # 6
+        )
+    }
+    aggLevelToNormalize <- function(agg_level) {
+        switch(agg_level,
+            c("iso"),                                          # 1
+            c("iso", "agg_fuel"),                              # 2
+            c("iso", "agg_fuel", "agg_sector"),                # 3
+            c("iso", "agg_fuel", "agg_sector", "CEDS_sector"), # 4
+            c("iso", "agg_fuel", "agg_sector"),                # 5
+            c("iso", "agg_fuel")                               # 6
+        )
     }
 
-# For each aggregate level, column names are specified that will guide
-# normalization and processing. Because of this flexible architecture, specific
-# aggregate levels can be easily adjusted or added without changing the design
-# of the function. However, there may only be one column in cols_given that
-# isn't present in normalizeTo; you can only ever normalize by a single "level"
-# of aggregation. This is a law of the function, not of the process, and may be
-# worth changing down the line.
-    if ( agg_level == 1 ) {
-        override_normalization <- T
-        normalizeTo <- c( "iso" ) # This will not be used at level 1
-                                  # but is necessary for creating the identifier_col
-        cols_given <- c( "agg_fuel", "iso" )
-    } else if ( agg_level == 2 ) {
-        normalizeTo <- c( "agg_fuel", "iso" )
-        cols_given <- c( "agg_fuel", "CEDS_fuel", "iso" )
-    } else if ( agg_level == 3 ) {
-        normalizeTo <- c( "agg_fuel", "agg_sector", "iso" )
-        cols_given <- c( "agg_fuel", "CEDS_fuel", "agg_sector", "iso" )
-    } else if ( agg_level == 4 ) {
-        normalizeTo <- c( "agg_fuel", "agg_sector", "CEDS_sector", "iso" )
-        cols_given <- c( "agg_fuel", "CEDS_fuel", "agg_sector",
-                         "CEDS_sector", "iso" )
-    } else if ( agg_level == 5 ) {
-        normalizeTo <- c( "agg_fuel", "agg_sector", "iso" )
-        cols_given <- c( "agg_fuel", "agg_sector", "CEDS_sector", "iso" )
-    } else if ( agg_level == 6 ) {
-        normalizeTo <- c( "agg_fuel", "iso" )
-        cols_given <- c( "agg_fuel", "agg_sector", "iso" )
-    } else {
-        stop( paste0( "agg_level ", agg_level, " not supported" ) )
-    }
+    cols_given <- aggLevelToCols(agg_level)
+    if(is.null(cols_given)) stop( paste( "agg_level", agg_level, "not supported" ) )
 
-# The "identifier column" is that column that will be unique among all
-# rows once aggregated to this level.
-    identifier_col <- cols_given[ which( cols_given %!in% normalizeTo)]
+    # This will not be used at level 1 but is necessary for creating the identifier_col
+    normalizeTo <- aggLevelToNormalize(agg_level)
 
-# Aggregate the pre-update data to match the user data level of aggregation
+    # The "identifier column" is the column that will be unique among all rows
+    # once we have aggregated to this level.
+    id_col <- dplyr::setdiff(cols_given, normalizeTo)
+
+    # Aggregate the pre-update data to match the user data level of aggregation
     activity_agg_to_level <- ddply( data_to_use, cols_given,
-                                     function(x) colSums( x[ Xyears ] ) )
+                                    function(x) colSums( x[ Xyears ] ) )
 
-# Add the user data (in a new copy of the agg dataframe)
+    # Add the user data (in a new copy of the agg dataframe)
+    rows_to_replace <- activity_agg_to_level[[id_col]] %in% user_dataframe_subset[[id_col]]
     act_agg_changed <- activity_agg_to_level
-    act_agg_changed[ which( act_agg_changed[[identifier_col]] %in%
-                                   user_dataframe_subset[[identifier_col]] ), Xyears ] <-
-                user_dataframe_subset[ , Xyears ]
+    act_agg_changed[ rows_to_replace, Xyears ] <- user_dataframe_subset[ , Xyears ]
 
-# Separate out those original columns which are not directly changed
-    data_to_correct <- activity_agg_to_level[ which( activity_agg_to_level[[identifier_col]] %!in%
-                                                      user_dataframe_subset[[identifier_col]] ),
-                                                      c( cols_given, Xyears ) ]
-# If there are no rows that weren't directly changed, then we've specified a
-# whole group (data for an entire aggregate group) and can't normalize
-    whole_group <- ( nrow( data_to_correct ) == 0 )
+    # Separate out those original columns which are not directly changed
+    data_to_correct <- activity_agg_to_level[ !rows_to_replace, c( cols_given, Xyears ) ]
 
-# Initialize warning info
+    # If all the rows were directly changed then we've specified data for an
+    # entire aggregate group and therefore can't normalize
+    whole_group <- all( rows_to_replace )
+
+    # Initialize warning info
     negatives <- F
     all_zero_years <- NA
     need_user_spec <- F
+
 # The block of code that deals with normalization (the process of modifying rows
 # that weren't directly specified in order to retain aggregate information) will
-# only be called if a) a whole group was not specified and b) no manual override
-# of normalization was called.
-    if ( !whole_group && !any( override_normalization ) ) {
+# only be called if
+#   a) a whole group was not specified and
+#   b) no manual override of normalization was called.
+    if ( !whole_group & !override_normalization ) {
 
         pct_of_agg_group <- data_to_correct
 
-    # Create a vector containing unaltered sums of activity in the group for each year
-        if ( length( Xyears ) > 1 ) {
-            year_totals <- colSums( activity_agg_to_level[ , Xyears ] )
-            year_totals_user_rows <- colSums( activity_agg_to_level[ which( activity_agg_to_level[[ identifier_col ]] %in%
-                                                         user_dataframe_subset[[ identifier_col ]] ), Xyears ] )
-        } else {
-        # due to a flaw in colSums, instances where there is only one column are separated
-            year_totals <- sum( activity_agg_to_level[ , Xyears ] )
-            year_totals_user_rows <- sum( activity_agg_to_level[ which( activity_agg_to_level[[identifier_col]] %in%
-                                                         user_dataframe_subset[[identifier_col]] ), Xyears ] )
-        }
+        # Create a vector containing unaltered sums of activity in the group for
+        # each year. drop = F keeps data as dataframe even if indexing one year.
+        year_totals <- colSums( activity_agg_to_level[ , Xyears, drop = F ] )
+        year_totals_user_rows <- colSums( activity_agg_to_level[ rows_to_replace, Xyears, drop = F ] )
 
     # Find the sum of only rows which need to be normalized (were not specified)
         year_totals_non_user_rows <- pct_of_agg_group
@@ -129,18 +119,21 @@ normalizeAndIncludeData <- function( Xyears, data_to_use, user_dataframe_subset,
                                         data.matrix( year_totals_non_user_rows[ , Xyears ] )
         pct_of_agg_group[ is.nan.df( pct_of_agg_group ) ] <- 0
 
-    # IF all of the data in a year (that wasn't user-specified) is 0, this is
-    # like having a "whole group"--we do not need to (cannot) normalize in this
-    # case. We will not compare these rows in the warnings check.
-        all_zero_years <- colnames( pct_of_agg_group[, Xyears] ) [ which( colSums(pct_of_agg_group[ , Xyears] == 0)
-                                                                          == nrow(pct_of_agg_group) ) ]
+        test <- data_to_correct[ , Xyears ] %>% as.matrix()
+        test <- prop.table(test, margin = 2)
+        test[is.nan(test)] <- 0
+        test <- cbind(data_to_correct[ , cols_given ], test)
+        identical(test, pct_of_agg_group)
+        pct_of_agg_group <- test
 
-    # Calculate the sum of the data post-inclusion and pre-normalization
-        if ( length( Xyears ) > 1 ) {
-            new_year_totals <- colSums( act_agg_changed[ , Xyears ] )
-        } else {
-            new_year_totals <- sum( act_agg_changed[ , Xyears ] )
-        }
+        # If all of the data in a year (that wasn't user-specified) is 0, this
+        # is like having a "whole group" -- we do not need to (cannot) normalize
+        # in this case. We will not compare these rows in the warnings check.
+        all_zero_years <- colSums( pct_of_agg_group[ , Xyears, drop = F ] ) == 0
+        all_zero_years <- names( which( all_zero_years ) )
+
+        # Calculate the sum of the data post-inclusion and pre-normalization
+        new_year_totals <- colSums( act_agg_changed[ , Xyears, drop = F ] )
 
     # Likely, adding new data will change the sum of the aggregate group; the
     # point of normalization is to eliminate this change. Annual diffs stores
@@ -177,23 +170,18 @@ normalizeAndIncludeData <- function( Xyears, data_to_use, user_dataframe_subset,
 
     # This dataframe now contains the user-defined data and normalized versions
     # of the undefined rows
-        act_agg_changed[ which( act_agg_changed[[ identifier_col ]] %!in%
-                                  user_dataframe_subset[[ identifier_col ]]), ] <- corrected_data
+        act_agg_changed[ which( act_agg_changed[[ id_col ]] %!in%
+                                  user_dataframe_subset[[ id_col ]]), ] <- corrected_data
     }
 
     disagg_data_changed <- data_to_use
-# Create a dataframe of group totals arranged correspondingly to the
-# disaggregate data
-    agg_group_totals_unchanged <- left_join( disagg_data_changed[ , c( "iso", "agg_fuel",
-                                                             "CEDS_fuel", "agg_sector",
-                                                             "CEDS_sector" ) ],
-                                   activity_agg_to_level,
-                                   by = cols_given )
-    agg_group_totals_changed <- left_join( disagg_data_changed[ , c( "iso", "agg_fuel",
-                                                             "CEDS_fuel", "agg_sector",
-                                                             "CEDS_sector" ) ],
-                                   act_agg_changed,
-                                   by = cols_given )
+    # Create a dataframe of group totals arranged correspondingly to the
+    # disaggregate data
+    join_cols <- c( "iso", "agg_fuel", "CEDS_fuel", "agg_sector", "CEDS_sector" )
+    agg_group_totals_unchanged <- left_join( disagg_data_changed[ , join_cols ],
+                                             activity_agg_to_level, by = cols_given )
+    agg_group_totals_changed   <- left_join( disagg_data_changed[ , join_cols ],
+                                             act_agg_changed, by = cols_given )
 
 # Calculate the percentage of the aggregate group that each row used to make up
     disagg_pct_breakdown <- data_to_use
@@ -206,7 +194,7 @@ normalizeAndIncludeData <- function( Xyears, data_to_use, user_dataframe_subset,
 # would fail to replace zero-emissions cells, because they would be
 # disaggregated using factors of zero. It creates percent breakdowns one of a
 # few ways; they will be explained as they appear (all contained in the
-# following if statement)s
+# following if statements).
     if ( any( activity_agg_to_level[ , Xyears ] == 0 &
               act_agg_changed[ , Xyears ] != 0 ) ) {
 
@@ -358,12 +346,16 @@ normalizeAndIncludeData <- function( Xyears, data_to_use, user_dataframe_subset,
 
 
     if ( any( specified_breakdowns ) ) {
-        user_breakdown <- NA
-        tryCatch( { user_breakdown <- readData( paste0( filename, "-breakdowns" ),
-                                                domain = "EXT_IN",
-                                                domain_extension = "user-defined-energy/" ) %>%
-                                      as.data.frame() },
-                  error = function(x) { message = "" } )
+        tryCatch({
+            user_breakdown <- readData( paste0( filename, "-breakdowns" ),
+                                        domain = "EXT_IN",
+                                        domain_extension = "user-defined-energy/" )
+            user_breakdown <- as.data.frame(user_breakdown)
+        }, error = function(e) {
+            stop( paste0( "No user specified breakdown found for ", filename ) )
+        })
+
+        # Do we still need this check?
         if ( !all( is.na( user_breakdown ) ) ) {
             if ( any( colnames( user_breakdown ) == "all" ) ) {
             # use the "all" column for every year
@@ -398,12 +390,8 @@ normalizeAndIncludeData <- function( Xyears, data_to_use, user_dataframe_subset,
                         correction_factors[ , Xyears ]
                disagg_pct_breakdown[ is.nan.df( disagg_pct_breakdown ) ]<- 0
             }
-        } else {
-            stop( paste0( "No user specified breakdown found for ", filename ) )
         }
-
     }
-
 
 
 # Multiply these percentages by the new values to get updated versions
@@ -463,7 +451,7 @@ generateWarnings <- function ( Xyears, disagg_data_changed,
     }
 
     warning_diag <- NA
-    if ( !any( override_normalization ) && !whole_group ) {
+    if ( !override_normalization && !whole_group ) {
         if ( length( years_to_compare ) > 1 ) {
             if ( any( round( colSums( disagg_data_changed[ , years_to_compare ] ), 3 ) !=
                       round( colSums( data_to_use[ , years_to_compare ] ), 3 ) ) ) {
@@ -487,7 +475,7 @@ generateWarnings <- function ( Xyears, disagg_data_changed,
               }
            }
         }
-    } else if ( any( override_normalization ) ) {
+    } else if ( override_normalization ) {
         warning_diag <- "Manual override of normalization"
     } else if ( whole_group ) {
         warning_diag <- "Whole group provided; aggregate sums overwritten"
@@ -591,19 +579,15 @@ initContinuityFactors <- function( activity, instructions, yearsAllowed,
         continuity_interval <- default_continuity_interval
     # Determine the years of the data
         data_years <- paste0( "X", start_year:end_year )
-        data_years <- data_years[ which( data_years %in%
-                                         colnames( activity$all_activity_data ) ) ]
+        data_years <- data_years[ data_years %in% names( activity$all_activity_data ) ]
     # If the data year length is less than twice the continuity interval, the interval will need to be reduced.
         if ( length( data_years ) < continuity_interval * 2 ) {
             continuity_interval <- floor( length( data_years ) / 2 )
         }
 
     # Determine which columns are present (bypass needing agg_level)
-        cols_given <- colnames( instructions )[ which( !is.na( this.row ) &
-                                                       colnames( instructions ) %in%
-                                                       c( "iso", "CEDS_fuel",
-                                                          "CEDS_sector", "agg_sector",
-                                                          "agg_fuel" ) ) ]
+        ok_cols <- c( "iso", "CEDS_fuel", "CEDS_sector", "agg_sector", "agg_fuel" )
+        cols_given <- names( instructions )[ !is.na( this.row ) & names( instructions ) %in% ok_cols ]
 
     # Extract the subset of disaggregated rows corresponding to the data
         rows_to_adjust <- activity$continuity_factors
@@ -619,16 +603,16 @@ initContinuityFactors <- function( activity, instructions, yearsAllowed,
 
         # If continuity enforcement is required at the beginning of this dataset:
         if ( this.row$start_continuity && start_year > historical_pre_extension_year ) {
-            intrvl_end <- start_year + continuity_interval - 1
-            year_range <- paste0( "X", start_year:intrvl_end )
+            interval_end <- start_year + continuity_interval - 1
+            year_range <- paste0( "X", start_year:interval_end )
             rows_to_adjust[ , year_range ] <- rep( continuity_vals, each = NROW( rows_to_adjust ) )
 
         }
 
         # If continuity enforcement is required at the end of this dataset:
         if ( this.row$end_continuity && end_year < historical_end_extension_year ) {
-            intrvl_end <- end_year - continuity_interval + 1
-            year_range <- paste0( "X", end_year:intrvl_end )
+            interval_end <- end_year - continuity_interval + 1
+            year_range <- paste0( "X", end_year:interval_end )
             rows_to_adjust[ , year_range ] <- rep( continuity_vals, each = NROW( rows_to_adjust ) )
         }
 
@@ -640,8 +624,7 @@ initContinuityFactors <- function( activity, instructions, yearsAllowed,
                         rows_to_adjust
 
     }
-# Return
-    return( activity )
 
+    return( activity )
 }
 
