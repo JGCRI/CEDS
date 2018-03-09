f091c1d2b7109ce412853b4e9e3737bcecc24431
Caleb Braun <caleb.braun@pnnl.gov>
2018-03-09 11:47:49 -0800

    Update parallel execution script

--- exe/PIC-job-scripts/submit-all-parallel.sh
+++ exe/PIC-job-scripts/submit-all-parallel.sh
@@ -14,3 +14,13 @@ sbatch make-OC.sh
 sbatch make-CH4.sh
 sbatch make-CO2.sh
 
+# Check once a minute if all the emissions are done
+while [ $( ls slurm-* 2>/dev/null | wc -l ) -le 1 ] || [ $( tail -n 1 slurm-* | grep 'Current time' | wc -l ) -ne $( ls slurm-* | wc -l ) ]
+do
+sleep 60s
+done
+
+# Do the non-parallel stuff
+cd ../../
+module load R
+Rscript code/module-S/S1.2.aggregate_summary_data.R
