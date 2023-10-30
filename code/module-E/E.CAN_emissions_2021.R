874bd2371ad946906b96583a47f3d914795a55b5
hahsan1 <72406931+hahsan1@users.noreply.github.com>
2023-10-30 16:49:04 -0400

    Fixed module E script for Canada emissions (BC and OC inventory generated using PM2.5, as well as a separate BC inventory from APEI data)

--- code/module-E/E.CAN_emissions_2021.R
+++ code/module-E/E.CAN_emissions_2021.R
@@ -30,17 +30,10 @@
   initialize( script_name, log_msg, headers )
 
 
-# Get emission species first so can name log appropriately
+  # Get emission species first so can name log appropriately
   args_from_makefile <- commandArgs( TRUE )
   em <- args_from_makefile[ 1 ]
   if ( is.na( em ) ) em <- "CH4"
-  em.read <- em
-  if( em == "NOx" ) em.read <- "NOx (t)"
-  if( em == "SO2" ) em.read <- "SOX (t)"
-  if( em == "NMVOC" ) em.read <- "VOC (t)"
-  if( em == "CO" ) em.read <- "CO (t)"
-  if( em == "NH3" ) em.read <- "NH3 (t)"
-  if (em == "PM25") em.read <- "PM25 (t)"
 
   # ------------------------------------------------------------------------------
   # 1. Define parameters for inventory specific script
@@ -52,108 +45,137 @@
   # Because this data comes read in as reversed.
   inv_years_reversed <- c( 2021:1990 )
 
+  # Define function for processing raw data
 
-# ------------------------------------------------------------------------------
-# 2. Reformat raw data files
+  output_data <- function(em2) {
+      #read in the raw data
+      raw_data <- list() #empty list
+      raw_data_path <- paste0('emissions-inventories/Canada/raw_2021/',em2,'/')
+      temp <- list.files(path = raw_data_path, pattern = paste0('APEI*'))
+      temp <- temp[ !grepl("metadata", temp) ]
+      for(i in 1:length(temp)) {raw_data[[i]] <- assign(paste0(em2,'_',i),read.csv(file = paste0(raw_data_path,temp[i]), stringsAsFactors = FALSE)%>%
+                                                            select(Source = 1, everything()) %>%
+                                                            gather("year","value",-c('Source','Sector','SubSector')))}
+      #combine all the raw data into one table, remove duplicate rows and totals
+      #for each sector and subsector
+      raw_data_total <- bind_rows(raw_data) %>%
+          distinct() %>%
+          filter(Source != 'Grand total') %>%
+          filter(Sector != '') %>%
+          filter(SubSector != '')
+
+      #condense the Source and Sector columns into one column, get rid of the "/"
+      #and transform the values from characters to doubles
+      output_sector_sub_combined <- transform(raw_data_total, sector_subsector=paste(Source,Sector,sep="_"))
+      output_sector_sub_combined$sector_subsector <-gsub("/","",output_sector_sub_combined$sector_subsector, fixed=TRUE)
+      output_sector_sub_combined$value <- as.double(output_sector_sub_combined$value)
+
+      #Summarise each sector_subsector column, convert to kt, and get into wide form
+      output_final <- output_sector_sub_combined %>%
+          mutate(value = value / 1000,
+                 iso = 'can') %>%
+          dplyr::rename(sector = sector_subsector) %>%
+          dplyr::group_by(iso,sector,year) %>%
+          dplyr::summarise(value = sum(value)) %>%
+          spread(key = 'year', value = 'value')
+
+      return(output_final)
+  }
+
+  # ------------------------------------------------------------------------------
+  # 2. Reformat raw data files
 
   #Makes sure only present data is processed
-  if( em %!in% c("CO2", "CH4", "N2O","OC") ){
-  #read in the raw data
-  raw_data <- list() #empty list
-  raw_data_path <- paste0('emissions-inventories/Canada/raw_2021/',em,'/')
-  temp <- list.files(path = raw_data_path, pattern = paste0('APEI*'))
-  temp <- temp[ !grepl("metadata", temp) ]
-  for(i in 1:length(temp)) {raw_data[[i]] <- assign(paste0(em,'_',i),read.csv(file = paste0(raw_data_path,temp[i]), stringsAsFactors = FALSE)%>%
-                                                      select(Source = 1, everything()) %>%
-                                                      gather("year","value",-c('Source','Sector','SubSector')))}
-  #combine all the raw data into one table, remove duplicate rows and totals
-  #for each sector and subsector
-  raw_data_total <- bind_rows(raw_data) %>%
-    distinct() %>%
-    filter(Source != 'Grand total') %>%
-    filter(Sector != '') %>%
-    filter(SubSector != '')
+  if( em %!in% c("CO2", "CH4", "N2O") ){
 
-  #condense the Source and Sector columns into one column, get rid of the "/"
-  #and transform the values from characters to doubles
-  output_sector_sub_combined <- transform(raw_data_total, sector_subsector=paste(Source,Sector,sep="_"))
-  output_sector_sub_combined$sector_subsector <-gsub("/","",output_sector_sub_combined$sector_subsector, fixed=TRUE)
-  output_sector_sub_combined$value <- as.double(output_sector_sub_combined$value)
+      if (em %in% c ('BC','OC')) {
 
-  #Summarise each sector_subsector column, convert to kt, and get into wide form
-  output_final <- output_sector_sub_combined %>%
-    mutate(value = value / 1000,
-      iso = 'can') %>%
-    dplyr::rename(sector = sector_subsector) %>%
-    dplyr::group_by(iso,sector,year) %>%
-    dplyr::summarise(value = sum(value)) %>%
-    spread(key = 'year', value = 'value')
+          # Write out the APEI BC inventory country total, which will be different than
+          # the BC estimate using PM2.5 data
+          if (em == "BC") {
+
+              country_total <- output_data(em)
+              country_total <- country_total %>%
+                  filter( !sector %in% c("Fires_Prescribed Burning",
+                                         "Fires_Structural Fires",
+                                         "Transportation and Mobile Equipment_Air Transportation",
+                                         "Transportation and Mobile Equipment_Marine Transportation"
+                  )) %>%
+                  ungroup() %>%
+                  dplyr::select(-sector) %>%
+                  group_by(iso) %>%
+                  summarise_at(vars(-group_cols(),),sum)
+
+              writeData( country_total, domain = "MED_OUT",
+                         paste0('E.',em,'_', inv_name, '_APEI_inventory_country_total'))
+
+          }
+
+          em2 <- 'PM25'
+      } else {em2 <- em}
+
+      output_final <- output_data(em2)
+
+      # Find BC and OC emissions
+
+      # Define parameters for BC and OC specific script
+      # Read in scaling mapping file and filter transportation sectors
+      ceds_sector <- "1A3b_Road"
+      inv_iso <- "can"
+      PM <- "PM25"
+      mapping_file <- readData("SCALE_MAPPINGS", "CAN_2018_scaling_mapping.csv")
+      mapping_file <- mapping_file %>%
+          filter(str_detect(road_flag,"Road"))
+
+      inv_sector_name <- mapping_file$inv_sector
+      X_inv_years <- paste0("X",inv_years) # put inventory years in BC/OC script format
+      inv_data_sheet <- output_final       # rename for BC/OC script input
+
+      # Calculate BC and OC emissions
+
+      if (em %in% c("BC","OC") ) {
+
+          output_final <- F.Estimate_BC_OC_emissions(em, PM, inv_iso,ceds_sector,inv_sector_name,X_inv_years)
+
+      }
+
   } else {Note <- c( "No CAN data available." )
   output_final <- dplyr::tibble( Note )}
 
 
-# Find BC and OC emissions
+  # 3. Write standard form inventory-
 
-    # Define parameters for BC and OC specific script
-    # Read in scaling mapping file and filter transportation sectors
-    ceds_sector <- "1A3b_Road"
-    inv_iso <- "can"
-    PM <- "PM25"
-    mapping_file <- readData("SCALE_MAPPINGS", "CAN_2018_scaling_mapping.csv")
-    mapping_file <- mapping_file %>%
-      filter(str_detect(road_flag,"Road"))
-
-    inv_sector_name <- mapping_file$inv_sector
-
-    X_inv_years <- paste0("X",inv_years) # put inventory years in BC/OC script format
-    inv_data_sheet <- output_final       # rename for BC/OC script input
-
-
-    # Calculate BC and OC emissions
-
-    if (em %in% c("BC","OC") ) {
-
-        em_emissions <- F.Estimate_BC_OC_emissions(em, PM, inv_iso,ceds_sector,inv_sector_name,X_inv_years)
-        output_final <- em_emissions
-
-    }
-
-
-# 3. Write standard form inventory-
-
-    writeData(output_final, domain = "MED_OUT",
-              paste0('E.', em, '_', inv_name, '_inventory'))
+  writeData(output_final, domain = "MED_OUT",
+            paste0('E.', em, '_', inv_name, '_inventory'))
 
 
   # Write out inventory country totals
-    # remove sectors for CEDS comparison
+  # remove sectors for CEDS comparison
   if (length(output_final)>1 ) {
       country_total <- output_final %>%
-      filter( !sector %in% c("Fires_Prescribed Burning",
-                             "Fires_Structural Fires",
-                             "Transportation and Mobile Equipment_Air Transportation",
-                             "Transportation and Mobile Equipment_Marine Transportation"
-                             ))
+          filter( !sector %in% c("Fires_Prescribed Burning",
+                                 "Fires_Structural Fires",
+                                 "Transportation and Mobile Equipment_Air Transportation",
+                                 "Transportation and Mobile Equipment_Marine Transportation"
+          ))
 
       writeData( country_total, domain = "DIAG_OUT", domain_extension = "country-inventory-compare/",
                  paste0('inventory_',em,'_', inv_name))
 
       #get rid of "sector" column and sum all the yearly data together
       country_total <- country_total %>%
-        ungroup() %>%
-        dplyr::select(-sector) %>%
-        group_by(iso) %>%
-        summarise_at(vars(-group_cols(),),sum)
+          ungroup() %>%
+          dplyr::select(-sector) %>%
+          group_by(iso) %>%
+          summarise_at(vars(-group_cols(),),sum)
 
-    writeData( country_total, domain = "MED_OUT",
-               paste0('E.',em,'_', inv_name, '_inventory_country_total'))
+      writeData( country_total, domain = "MED_OUT",
+                 paste0('E.',em,'_', inv_name, '_inventory_country_total'))
 
-    } else {Note <- c( "No CAN data available." )
-    output_final <- dplyr::tibble( Note )}
-
-
-# Every script should finish with this line
-    logStop()
-    # END
+  } else {Note <- c( "No CAN data available." )
+  output_final <- dplyr::tibble( Note )}
 
 
+  # Every script should finish with this line
+  logStop()
+  # END
