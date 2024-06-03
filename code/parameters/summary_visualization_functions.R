21778fe46d1c411f100a90e75e2a6f3ed49e24f8
Rachel Hoesly <rhoesly@gmail.com>
2024-06-02 22:48:35 -0400

    add early and mid year summary visualizations (so its not only modern era in the comparison graphs)

--- code/parameters/summary_visualization_functions.R
+++ code/parameters/summary_visualization_functions.R
@@ -48,8 +48,8 @@ year_breaks <- 10
 theme_diagnostic <- list(theme(panel.background = element_rect(fill = "gray90",colour = "gray90",size = 0.5, linetype = "solid")),
                          theme(panel.grid.major = element_line(size = 0.5, linetype = 'solid',colour = "gray95")),
                          theme(panel.grid.minor = element_line(size = 0.25, linetype = 'solid',colour = "white")))
-
-version_comparison_formatting <- list(
+# Formatting for the 3 different time scales
+version_comparison_formatting_modern <- list(
     scale_x_continuous( breaks = seq( from = graph_start, to = graph_end, by = year_breaks ) ,
                     limits = c(1960, graph_end+1)) ,
     #scale_y_continuous( labels = 'comma' ),
@@ -59,6 +59,30 @@ version_comparison_formatting <- list(
     scale_linetype_manual( name= 'Version',
                        breaks = c( 'Last' ,'Current'),
                        values = c( 'solid','solid' ) ) )
+
+version_comparison_formatting_mid <- list(
+    scale_x_continuous( breaks = seq( from = graph_start, to = graph_end, by = year_breaks ) ,
+                        limits = c(1900, 1970)) ,
+    #scale_y_continuous( labels = 'comma' ),
+    guides( linetype = guide_legend( override.aes = list( size = c( 1.5, 0.5 ) ) ) ),
+    labs( x = "" , y = 'Emissions [Tg/yr]' ),
+    ggtitle(EM),
+    scale_linetype_manual( name= 'Version',
+                           breaks = c( 'Last' ,'Current'),
+                           values = c( 'solid','solid' ) ) )
+
+version_comparison_formatting_early <- list(
+    scale_x_continuous( breaks = seq( from = graph_start, to = graph_end, by = 25 ) ,
+                        limits = c(1750, 1910)) ,
+    #scale_y_continuous( labels = 'comma' ),
+    guides( linetype = guide_legend( override.aes = list( size = c( 1.5, 0.5 ) ) ) ),
+    labs( x = "" , y = 'Emissions [Tg/yr]' ),
+    ggtitle(EM),
+    scale_linetype_manual( name= 'Version',
+                           breaks = c( 'Last' ,'Current'),
+                           values = c( 'solid','solid' ) ) )
+
+
 
 # Read in current data and previous data
 # Some of this is already loaded into the the summary script, but just re-read
@@ -162,7 +186,7 @@ plot_global_sectors <- ggplot(dfplot_global_sectors, aes(x = year, y = value, co
                size = 2 ,aes( x = year, y= value, color = Sector ), alpha = .4 ) +
     geom_line( data = dplyr::filter( dfplot_global_sectors, Version == 'Current'  ),
                size = 0.5, aes( x = year, y = value, color = Sector ), alpha = 1 ) +
-    version_comparison_formatting+
+    version_comparison_formatting_modern+
     theme_diagnostic
 
 plot_global_fuel <- ggplot(dfplot_global_fuel, aes(x = year, y = value, color = Fuel, linetype= Version))+
@@ -170,7 +194,7 @@ plot_global_fuel <- ggplot(dfplot_global_fuel, aes(x = year, y = value, color = 
                size = 2 ,aes( x = year, y= value, color = Fuel ), alpha = .4 ) +
     geom_line( data = dplyr::filter( dfplot_global_fuel, Version == 'Current'  ),
                size = 0.5, aes( x = year, y = value, color = Fuel ), alpha = 1 ) +
-    version_comparison_formatting+
+    version_comparison_formatting_modern+
     theme_diagnostic
 
 plot_global_region <- ggplot(dfplot_global_region, aes(x = year, y = value, color = Region, linetype= Version))+
@@ -178,7 +202,7 @@ plot_global_region <- ggplot(dfplot_global_region, aes(x = year, y = value, colo
                size = 2 ,aes( x = year, y= value, color = Region ), alpha = .4 ) +
     geom_line( data = dplyr::filter( dfplot_global_region, Version == 'Current'  ),
                size = 0.5, aes( x = year, y = value, color = Region ), alpha = 1 ) +
-    version_comparison_formatting+
+    version_comparison_formatting_modern+
     theme_diagnostic
 
 plot_global_total <- ggplot(dfplot_global_total, aes(x = year, y = value,  linetype= Version))+
@@ -186,19 +210,36 @@ plot_global_total <- ggplot(dfplot_global_total, aes(x = year, y = value,  linet
                size = 2 ,aes( x = year, y= value), alpha = .4 , color = global_color) +
     geom_line( data = dplyr::filter( dfplot_global_total, Version == 'Current'  ),
                size = 0.5, aes( x = year, y = value ), alpha = 1 , color = global_color) +
-    version_comparison_formatting+
+    version_comparison_formatting_modern+
     theme_diagnostic
 
 # Plot Regions
 plot_region <- function(df_region){
 
-    ggplot(df_region, aes(x = year, y = value, color = Sector, linetype= Version))+
+   modern <-  ggplot(df_region, aes(x = year, y = value, color = Sector, linetype= Version))+
         geom_line( data = dplyr::filter( df_region, Version == 'Last' ),
                    size = 2 ,aes( x = year, y= value, color = Sector ), alpha = .4 ) +
         geom_line( data = dplyr::filter( df_region, Version == 'Current'  ),
                    size = 0.5, aes( x = year, y = value, color = Sector ), alpha = 1 ) +
-        version_comparison_formatting+
+        version_comparison_formatting_modern+
         theme_diagnostic
+   mid <-  ggplot(df_region, aes(x = year, y = value, color = Sector, linetype= Version))+
+        geom_line( data = dplyr::filter( df_region, Version == 'Last' ),
+                   size = 2 ,aes( x = year, y= value, color = Sector ), alpha = .4 ) +
+        geom_line( data = dplyr::filter( df_region, Version == 'Current'  ),
+                   size = 0.5, aes( x = year, y = value, color = Sector ), alpha = 1 ) +
+        version_comparison_formatting_mid+
+        theme_diagnostic
+  early <- ggplot(df_region, aes(x = year, y = value, color = Sector, linetype= Version))+
+        geom_line( data = dplyr::filter( df_region, Version == 'Last' ),
+                   size = 2 ,aes( x = year, y= value, color = Sector ), alpha = .4 ) +
+        geom_line( data = dplyr::filter( df_region, Version == 'Current'  ),
+                   size = 0.5, aes( x = year, y = value, color = Sector ), alpha = 1 ) +
+        version_comparison_formatting_early+
+        theme_diagnostic
+
+  out <- list(modern, mid, early)
+  return(out)
 }
 
 region_plots <- lapply(list(dfplot_Africa_sectors, dfplot_China_sectors, dfplot_North_America_sectors, dfplot_Europe_sectors, dfplot_FSU_sectors, dfplot_Latin_America_sectors, dfplot_Other_sectors),
@@ -206,9 +247,31 @@ region_plots <- lapply(list(dfplot_Africa_sectors, dfplot_China_sectors, dfplot_
 
 #output
 
-summary_plot_output <- c(list(plot_global_sectors, plot_global_fuel, plot_global_region, plot_global_total), region_plots)
-names(summary_plot_output) <- c("Global Sectors",'Global Fuel', 'Global Region', 'Global Total',
-                                "Africa", "China", "North America", "Europe", "FSU", "Latin America", "Other Asia/Pacific")
+summary_plot_output <- c(list(plot_global_sectors,
+                                plot_global_sectors+version_comparison_formatting_mid,
+                                plot_global_sectors+version_comparison_formatting_early,
+                              plot_global_fuel,
+                                plot_global_fuel+version_comparison_formatting_mid,
+                                plot_global_fuel+version_comparison_formatting_early,
+                              plot_global_region,
+                                plot_global_region+version_comparison_formatting_mid,
+                                plot_global_region+version_comparison_formatting_early,
+                              plot_global_total,
+                                plot_global_total+version_comparison_formatting_mid,
+                                plot_global_total+version_comparison_formatting_early),
+                         unlist(region_plots, recursive = FALSE))
+
+names(summary_plot_output) <- c('Global Sectors - Modern','Global Sectors - Mid', 'Global Sectors - Early',
+                                'Global Fuel - Modern', 'Global Fuel - Mid','Global Fuel - Early',
+                                'Global Region - Modern', 'Global Region - Mid', 'Global Region - Early',
+                                'Global Total - Modern','Global Total - Mid','Global Total - Early',
+                                "Africa - Modern", "Africa - Mid","Africa - Early",
+                                "China - Modern", "China - Mid", "China - Early",
+                                "North America - Modern", "North America - Mid", "North America - Early",
+                                "Europe - Modern", "Europe - Mid", "Europe - Early",
+                                "FSU - Modern", "FSU - Mid","FSU - Early",
+                                "Latin America - Modern", "Latin America - Mid","Latin America - Early",
+                                "Other Asia/Pacific - Modern","Other Asia/Pacific - Mid","Other Asia/Pacific - Early")
 
 # If individual country(ies) selected:
 #Plot one graph with all country totals
@@ -230,13 +293,17 @@ if(!is.na(country_select)){
                    size = 2 ,aes( x = year, y= value, color = Country ), alpha = .4 ) +
         geom_line( data = dplyr::filter( dfplot_all_countries, Version == 'Current'  ),
                    size = 0.5, aes( x = year, y = value, color = Country ), alpha = 1 ) +
-        version_comparison_formatting+
+        version_comparison_formatting_modern+
         theme_diagnostic
 
 # Redefine lists of plots for output
 last_index <- length(summary_plot_output)
 summary_plot_output[[(last_index+1)]] <- plot_all_countries
-names(summary_plot_output)[(last_index+1)] <- "Selected Countries"
+summary_plot_output[[(last_index+2)]] <- plot_all_countries + version_comparison_formatting_mid
+summary_plot_output[[(last_index+3)]] <- plot_all_countries + version_comparison_formatting_early
+names(summary_plot_output)[(last_index+1)] <- "Selected Countries - Modern"
+names(summary_plot_output)[(last_index+2)] <- "Selected Countries - Mid"
+names(summary_plot_output)[(last_index+3)] <- "Selected Countries - Early"
 
 #Plot individual country graphs by sector
 dfplot_countries_sectors <- current_Em_by_Country_Sector %>%  mutate(Version = 'Current') %>%
@@ -260,12 +327,16 @@ plot_country <- ggplot(dfplot_country, aes(x = year, y = value, color = Sector, 
                size = 2 ,aes( x = year, y= value, color = Sector ), alpha = .4 ) +
     geom_line( data = dplyr::filter( dfplot_country, Version == 'Current'  ),
                size = 0.5, aes( x = year, y = value, color = Sector ), alpha = 1 ) +
-    version_comparison_formatting+
+    version_comparison_formatting_modern+
     theme_diagnostic
 
 last_index <- length(summary_plot_output)
 summary_plot_output[[(last_index+1)]] <- plot_country
-names(summary_plot_output)[(last_index+1)] <- country_select[i]
+summary_plot_output[[(last_index+2)]] <- plot_country + version_comparison_formatting_mid
+summary_plot_output[[(last_index+3)]] <- plot_country + version_comparison_formatting_early
+names(summary_plot_output)[(last_index+1)] <- paste0( country_select[i], ' - Modern')
+names(summary_plot_output)[(last_index+2)] <- paste0( country_select[i], ' - Mid')
+names(summary_plot_output)[(last_index+3)] <- paste0( country_select[i], ' - Early')
 
 }}
 
@@ -297,33 +368,75 @@ print_single_em_comparison_plots <- function(plot_list = comparison_plots,
     pdf( paste0( "../final-emissions/diagnostics/Comparison_Plots_",EM,'.pdf' ),
          width = 14, height = 10, paper ='special' )
 
-    grid.arrange( plot_list[["Global Sectors"]]+ggtitle('Global Sectors'),
-                  plot_list[["Global Fuel"]]+ggtitle('Global Fuel'),
-                  plot_list[["Global Region"]]+ggtitle('Global Region'),
-                  plot_list[["Global Total"]]+ggtitle('Global Total'),
+    grid.arrange( plot_list[["Global Sectors - Modern"]]+ggtitle('Global Sectors'),
+                  plot_list[["Global Fuel - Modern"]]+ggtitle('Global Fuel'),
+                  plot_list[["Global Region - Modern"]]+ggtitle('Global Region'),
+                  plot_list[["Global Total - Modern"]]+ggtitle('Global Total'),
                   ncol = 3, nrow = 3,padding = unit(3, "line"),
                   top = paste0( "Global CEDS comparison - current to previous version" ) )
 
-    grid.arrange( plot_list[["Africa"]]+ggtitle('Africa'),
-                  plot_list[["China"]]+ggtitle('China'),
-                  plot_list[["North America"]]+ggtitle('North America'),
-                  plot_list[["Europe"]]+ggtitle('Europe'),
-                  plot_list[["FSU"]]+ggtitle('FSU'),
-                  plot_list[["Latin America"]]+ggtitle('Latin America'),
-                  plot_list[["Other Asia/Pacific"]]+ggtitle('Other Asia/Pacific'),
+    grid.arrange( plot_list[["Global Sectors - Mid"]]+ggtitle('Global Sectors'),
+                  plot_list[["Global Fuel - Mid"]]+ggtitle('Global Fuel'),
+                  plot_list[["Global Region - Mid"]]+ggtitle('Global Region'),
+                  plot_list[["Global Total - Mid"]]+ggtitle('Global Total'),
+                  ncol = 3, nrow = 3,padding = unit(3, "line"),
+                  top = paste0( "Global CEDS comparison - current to previous version" ) )
+
+    grid.arrange( plot_list[["Global Sectors - Early"]]+ggtitle('Global Sectors'),
+                  plot_list[["Global Fuel - Early"]]+ggtitle('Global Fuel'),
+                  plot_list[["Global Region - Early"]]+ggtitle('Global Region'),
+                  plot_list[["Global Total - Early"]]+ggtitle('Global Total'),
+                  ncol = 3, nrow = 3,padding = unit(3, "line"),
+                  top = paste0( "Global CEDS comparison - current to previous version" ) )
+
+    grid.arrange( plot_list[["Africa - Modern"]]+ggtitle('Africa'),
+                  plot_list[["China - Modern"]]+ggtitle('China'),
+                  plot_list[["North America - Modern"]]+ggtitle('North America'),
+                  plot_list[["Europe - Modern"]]+ggtitle('Europe'),
+                  plot_list[["FSU - Modern"]]+ggtitle('FSU'),
+                  plot_list[["Latin America - Modern"]]+ggtitle('Latin America'),
+                  plot_list[["Other Asia/Pacific - Modern"]]+ggtitle('Other Asia/Pacific'),
+                  ncol = 3, nrow = 3,
+                  top = paste0( "Regional CEDS comparison - current to previous version" ) )
+
+    grid.arrange( plot_list[["Africa - Mid"]]+ggtitle('Africa'),
+                  plot_list[["China - Mid"]]+ggtitle('China'),
+                  plot_list[["North America - Mid"]]+ggtitle('North America'),
+                  plot_list[["Europe - Mid"]]+ggtitle('Europe'),
+                  plot_list[["FSU - Mid"]]+ggtitle('FSU'),
+                  plot_list[["Latin America - Mid"]]+ggtitle('Latin America'),
+                  plot_list[["Other Asia/Pacific - Mid"]]+ggtitle('Other Asia/Pacific'),
+                  ncol = 3, nrow = 3,
+                  top = paste0( "Regional CEDS comparison - current to previous version" ) )
+
+    grid.arrange( plot_list[["Africa - Early"]]+ggtitle('Africa'),
+                  plot_list[["China - Early"]]+ggtitle('China'),
+                  plot_list[["North America - Early"]]+ggtitle('North America'),
+                  plot_list[["Europe - Early"]]+ggtitle('Europe'),
+                  plot_list[["FSU - Early"]]+ggtitle('FSU'),
+                  plot_list[["Latin America - Early"]]+ggtitle('Latin America'),
+                  plot_list[["Other Asia/Pacific - Early"]]+ggtitle('Other Asia/Pacific'),
                   ncol = 3, nrow = 3,
                   top = paste0( "Regional CEDS comparison - current to previous version" ) )
 
     if(!is.na(CS)){
+        if(length(CS)>6)stop("individual countries selected is more than 6 - summary visualization can't support more than 6")
         index_last <- length(plot_list)
-        countries_plot_list <- plot_list[11:index_last]
+        countries_plot_list <- plot_list[37:index_last]
         list_last <- length(countries_plot_list)
-        for(i in 2:list_last){
+        for(i in 1:list_last){
             countries_plot_list[[i]] <- countries_plot_list[[i]]+ ggtitle(names(countries_plot_list)[i])
         }
-        do.call("grid.arrange", c(countries_plot_list, ncol=3,nrow=3,
+    if( length(countries_plot_list) > 9){
+        do.call("grid.arrange", c(countries_plot_list[1:9], ncol=3,nrow=3,
+                                  top = paste0( "Regional CEDS comparison - current to previous version" )))
+        do.call("grid.arrange", c(countries_plot_list[10: length(countries_plot_list) ], ncol=3,nrow=3,
                                   top = paste0( "Regional CEDS comparison - current to previous version" )))
 
+    }else(
+        do.call("grid.arrange", c(countries_plot_list, ncol=3,nrow=3,
+                                  top = paste0( "Regional CEDS comparison - current to previous version" )))
+    )
 }
     dev.off( )
 }
