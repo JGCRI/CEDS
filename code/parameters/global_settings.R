4231285fb6573f4e943009437cbda0f74d037e59
Rachel Hoesly <rhoesly@gmail.com>
2024-07-23 16:12:29 -0400

    July 08, 2024 Release - Comparison documents - Read me update - global settings manual version number tag

--- code/parameters/global_settings.R
+++ code/parameters/global_settings.R
@@ -84,7 +84,7 @@ VERBOSE <- TRUE
 #follow the instruction in the next comment
 
 # User should uncomment the following line if he/she has a specific cedsUserVersionNumber to use
- options(cedsUserVersionNumber = "v_2024_04_01") # Comment out to get dynamic version number
+# options(cedsUserVersionNumber = "v_2024_07_08") # Comment out to get dynamic version number
                                                 # (automatically sets the version tag to the
                                                 # date of the CEDS run)
 
